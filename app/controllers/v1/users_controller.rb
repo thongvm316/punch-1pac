b9f91798a3bca8f0c801bd10bce7b3cd2b90ac58
Thoi Duong <duong.thoi@1pac.vn>
2017-12-13 14:56:06 +0700

    Remove unuse code, reintent

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class V1::UsersController < ApplicationController
-  before_action :user_params, only: [:create]
   before_action :set_user, only: %i[update destroy]
 
   def index
@@ -19,10 +18,9 @@ class V1::UsersController < ApplicationController
   end
 
   def create_multi
-    results = User.import_csv(csv_params[:csv_file].tempfile, current_user.company.id)
+    results = User.import_csv(params[:csv_file].tempfile, current_user.company.id)
     users = ActiveModelSerializers::SerializableResource.new(results[:users], each_serializer: UserSerializer).as_json
-    errors = results[:errors]
-    render json: { users: users, errors: errors }, status: 200
+    render json: { users: users, errors: results[:errors] }, status: 200
   end
 
   def update
@@ -41,20 +39,12 @@ class V1::UsersController < ApplicationController
   private
 
   def user_params
-    u_params = params.require(:user).permit(:department_id,
-                                            :name,
-                                            :password,
-                                            :password_confirmation,
-                                            :email)
+    u_params = params.require(:user).permit(:department_id, :name, :password, :password_confirmation, :email)
     u_params.merge(company_id: current_user.company.id)
   end
 
   def update_user_params
     params.require(:user).permit(:name, :email)
-  end
-
-  def csv_params
-    params.permit(:csv_file)
   end
 
   def set_user
