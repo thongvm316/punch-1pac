2f3b2a6067532ceaa01d95d4e6c876ff48c41f95
Thoi Duong <duong.thoi@1pac.vn>
2017-12-29 16:09:46 +0700

    Update policy

--- app/policies/department_policy.rb
+++ app/policies/department_policy.rb
@@ -1,19 +1,3 @@
 # frozen_string_literal: true
 
-class DepartmentPolicy < ApplicationPolicy
-  def index?
-    @user.manager?
-  end
-
-  def show?
-    @user.manager?
-  end
-
-  def update?
-    @user.manager?
-  end
-
-  def create?
-    @user.manager?
-  end
-end
+class DepartmentPolicy < ApplicationPolicy; end
