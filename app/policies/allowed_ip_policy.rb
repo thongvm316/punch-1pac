456198aa05d1a1dfda14f89c0784241cbd5fc4d8
Thoi Duong <duong.thoi@1pac.vn>
2017-12-29 16:42:35 +0700

    Update policy

--- app/policies/allowed_ip_policy.rb
+++ app/policies/allowed_ip_policy.rb
@@ -1,19 +1,3 @@
 # frozen_string_literal: true
 
-class AllowedIpPolicy < ApplicationPolicy
-  def index?
-    @user.manager?
-  end
-
-  def create?
-    @user.manager?
-  end
-
-  def update?
-    @user.manager?
-  end
-
-  def destroy?
-    @user.manager?
-  end
-end
+class AllowedIpPolicy < ApplicationPolicy; end
