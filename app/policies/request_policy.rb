eb8dcd7c5ad9cc1d124ae948ce77d4bd72241279
Thoi Duong <duong.thoi@1pac.vn>
2017-12-29 18:03:49 +0700

    Add authorize

---
+++ app/policies/request_policy.rb
@@ -0,0 +1,17 @@
+class RequestPolicy < ApplicationPolicy
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def approve?
+    @user.manager?
+  end
+
+  def reject?
+    @user.manager?
+  end
+end
