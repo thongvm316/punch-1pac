af4dc549d2e6e6c35af6a160c8e9e11a03c8debc
Thoi Duong <duong.thoi@1pac.vn>
2017-12-29 16:40:51 +0700

    Update policy

--- app/policies/business_day_policy.rb
+++ app/policies/business_day_policy.rb
@@ -1,19 +1,3 @@
 # frozen_string_literal: true
 
-class BusinessDayPolicy < ApplicationPolicy
-  def index?
-    @user.manager?
-  end
-
-  def create?
-    @user.manager?
-  end
-
-  def update?
-    @user.manager?
-  end
-
-  def destroy?
-    @user.manager?
-  end
-end
+class BusinessDayPolicy < ApplicationPolicy; end
