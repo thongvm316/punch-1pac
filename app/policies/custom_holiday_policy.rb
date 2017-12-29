bc846e3a92fa441bc143c940bcb81020d4453e13
Thoi Duong <duong.thoi@1pac.vn>
2017-12-29 16:36:26 +0700

    Update policy

--- app/policies/custom_holiday_policy.rb
+++ app/policies/custom_holiday_policy.rb
@@ -1,19 +1,3 @@
 # frozen_string_literal: true
 
-class CustomHolidayPolicy < ApplicationPolicy
-  def index?
-    @user.manager?
-  end
-
-  def create?
-    @user.manager?
-  end
-
-  def update?
-    @user.manager?
-  end
-
-  def destroy?
-    @user.manager?
-  end
-end
+class CustomHolidayPolicy < ApplicationPolicy; end
