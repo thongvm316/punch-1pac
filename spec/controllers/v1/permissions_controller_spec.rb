585e68f515ec2d5fe41c9a8d34a58de1d317d449
Thoi Duong <duong.thoi@1pac.vn>
2018-01-03 15:09:00 +0700

    Modify test case

--- spec/controllers/v1/permissions_controller_spec.rb
+++ spec/controllers/v1/permissions_controller_spec.rb
@@ -20,9 +20,9 @@ RSpec.describe V1::PermissionsController, type: :controller do
     context 'when login user is admin' do
       let(:login_user) { create :user, company: company, role: 'admin' }
 
-      context 'when success' do
+      context 'when get all permission' do
         let!(:permissions) { create_list :permission, 5 }
-        let(:total_permissions) { permissions.count + login_user.permissions.count }
+        let(:total_permissions) { Permission.count }
 
         subject { get :index }
 
@@ -35,14 +35,14 @@ RSpec.describe V1::PermissionsController, type: :controller do
         let!(:permissions_role_1) { create_list :permission, 5, role: 3 }
         let!(:permissions_role_2) { create_list :permission, 5, role: 4 }
 
-        context 'when role = 1' do
+        context 'when role = 3' do
           subject { get :index, params: { role: 3 } }
 
           its(:code) { is_expected.to eq '200' }
           its(:body) { is_expected.to be_json_as(Array.new(permissions_role_1.count) { response_permission }) }
         end
 
-        context 'when role = 2' do
+        context 'when role = 4' do
           subject { get :index, params: { role: 4 } }
 
           its(:code) { is_expected.to eq '200' }
