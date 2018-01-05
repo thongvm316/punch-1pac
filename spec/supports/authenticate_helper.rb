5863f19eddfb96048cd07db1ae719a8f6a56d5a5
Minh Phan <wofi.minh@1pac.vn>
2018-01-05 10:41:41 +0700

    Add set namespace helper for spec

--- spec/supports/authenticate_helper.rb
+++ spec/supports/authenticate_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module AuthenticateHelper
+  def in_namespace(company)
+    @request.host = "#{company.namespace}.example.com"
+  end
+
   def authenticate_user(user)
     jti = SecureRandom.uuid
     exp = Time.current.to_i + 2.days.to_i
