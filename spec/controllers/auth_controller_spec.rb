bc7a60df57f14900c5740733894c9aa786dd4449
Minh Phan <wofi.minh@1pac.vn>
2018-01-08 00:40:51 +0700

    Refactor login/logout API

--- spec/controllers/auth_controller_spec.rb
+++ spec/controllers/auth_controller_spec.rb
@@ -4,28 +4,33 @@
 
 require 'rails_helper'
 
-RSpec.describe TokenController, type: :controller do
+RSpec.describe AuthController, type: :controller do
   let(:company) { create :company }
   let(:user) { create :user, company: company }
 
   before { in_namespace(company) }
 
+  describe 'GET #new' do
+  end
+
   describe 'POST #create' do
     context 'when email is wrong' do
-      subject { post :create, params: { email: user.email + 'fake', password: user.password } }
+      subject { post :create, params: { email: user.email + 'fake', password: user.password }, format: :json }
 
       its(:code) { is_expected.to eq '401' }
     end
 
     context 'when password is wrong' do
-      subject { post :create, params: { email: user.email, password: user.password + 'fake' } }
+      subject { post :create, params: { email: user.email, password: user.password + 'fake' }, format: :json }
 
       its(:code) { is_expected.to eq '401' }
     end
 
     context 'when email and password are correct' do
-      shared_examples 'a successful login' do
-        before { request.headers['User-Agent'] = Faker::Internet.user_agent(:chrome) }
+      before { request.headers['User-Agent'] = Faker::Internet.user_agent(:chrome) }
+
+      context 'when request from mobile' do
+        subject { post :create, params: { email: user.email, password: user.password }, format: :json }
 
         its(:code) { is_expected.to eq '200' }
         its(:body) { is_expected.to be_json_as(access_token: String) }
@@ -34,19 +39,13 @@ RSpec.describe TokenController, type: :controller do
         end
       end
 
-      context 'when request from mobile' do
+      context 'when request from browser' do
         subject { post :create, params: { email: user.email, password: user.password } }
 
-        it_behaves_like 'a successful login'
-      end
-
-      context 'when request from xhr' do
-        subject { post :create, params: { email: user.email, password: user.password }, xhr: true }
-
-        it_behaves_like 'a successful login'
-
-        it 'should contain access_token in session' do
-          is_expected
+        its(:code) { is_expected.to eq '302' }
+        it do
+          expect { subject }.to change(Session, :count).by(1)
+          expect(subject).to redirect_to('/dashboard')
           expect(session[:access_token]).not_to be_nil
         end
       end
@@ -55,21 +54,22 @@ RSpec.describe TokenController, type: :controller do
 
   describe 'DELETE #destroy' do
     context 'when request from mobile' do
-      subject { delete :destroy }
+      subject { delete :destroy, format: :json }
 
       before { authenticate_user(user) }
 
       its(:code) { is_expected.to eq '200' }
     end
 
-    context 'when request from xhr' do
-      subject { delete :destroy, xhr: true }
+    context 'when request from browser' do
+      subject { delete :destroy }
 
       before { authenticate_user(user) }
 
-      its(:code) { is_expected.to eq '200' }
+      its(:code) { is_expected.to eq '302' }
       it 'should delete access_token in session' do
         is_expected
+        expect(subject).to redirect_to('/')
         expect(session[:access_token]).to be_nil
       end
     end
