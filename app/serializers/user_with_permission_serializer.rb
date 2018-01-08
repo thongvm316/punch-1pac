3edf6ab906535603ab7d2e753190353087c9f385
Thoi Duong <duong.thoi@1pac.vn>
2018-01-08 10:11:11 +0700

    Add image uploader to user

--- app/serializers/user_with_permission_serializer.rb
+++ app/serializers/user_with_permission_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class UserWithPermissionSerializer < ApplicationSerializer
-  attributes :id, :email, :name
+  attributes :id, :email, :name, :avatar_url
   has_many :permissions
 end
---
+++ config/initializers/shrine.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'shrine/storage/file_system'
+
+Shrine.plugin :activerecord
+Shrine.plugin :logging, logger: Rails.logger
+
+Shrine.storages = {
+  cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'),
+  store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/store')
+}
