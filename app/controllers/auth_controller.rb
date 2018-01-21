58f725789cc20b298773b347f8a7e21b79f57d2e
Minh Phan <wofi.minh@1pac.vn>
2018-01-21 15:07:56 +0700

    Fix authentication process a bit

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 class AuthController < ApplicationController
+  layout 'static'
   before_action :current_company
   before_action :authenticate_user!, only: %i[destroy]
 
   def new
     redirect_to(url_for(subdomain: request.subdomain, controller: 'dashboard', action: 'index', path: 'dashboard')) && return if signed_in?
     @user = User.new
-    render :new, layout: 'static'
   end
 
   def create
     head(200) if signed_in?
-    @user = current_company.users.find_by(email: params[:email])
+    @user = current_company.users.find_by(email: auth_params[:email])
     respond_to do |f|
-      if @user&.authenticate(params[:password])
+      if @user&.authenticate(auth_params[:password])
         data = payload(sub: @user.id)
         token = jwt_encode(data)
         Session.track!(@user, data, request)
@@ -25,7 +25,8 @@ class AuthController < ApplicationController
         f.json { render json: { access_token: token }, status: 200 }
       else
         f.html do
-          @msg = 'Incorrect email or password'
+          @user = User.new(email: auth_params[:email])
+          flash.now[:alert] = 'Incorrect email or password'
           render :new
         end
         f.json { head(401) }
@@ -35,12 +36,13 @@ class AuthController < ApplicationController
 
   def destroy
     revoke_jwt!
-    respond_to do |f|
-      f.html do
-        session.delete(:access_token)
-        redirect_to login_url
-      end
-      f.json { render json: { access_token: token }, status: 200 }
-    end
+    session.delete(:access_token)
+    render json: { access_token: token }, status: 200
+  end
+
+  private
+
+  def auth_params
+    params.require(:user).permit(:email, :password)
   end
 end
