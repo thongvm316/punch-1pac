f9356909f6ee65d094fbe931f8abf60b2a222651
Minh Phan <wofi.minh@1pac.vn>
2018-01-22 17:19:59 +0700

    Fix dashboard_controller test

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,15 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
-  let(:company) { create :company }
-  let(:login_user) { create :user, company: company }
+  controller do
+    before_action :authenticate_user!
+    before_action :current_company
+    helper_method :current_user, :current_company
 
-  before do
-    in_namespace(company)
-    authenticate_user(login_user)
+    def index
+      @webpack_assets = {}
+    end
   end
 
   describe 'GET #index' do
+    let(:company) { create :company }
+    let(:login_user) { create :user, company: company }
+
+    before do
+      in_namespace(company)
+      authenticate_user(login_user)
+    end
+
     subject { get :index, params: { path: Faker::Lorem.word } }
 
     it { is_expected.to have_http_status(:success) }
