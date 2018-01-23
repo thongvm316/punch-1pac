08efce4bd63c80ef8a34a2c313363cc0db6372a1
Thoi Duong <duong.thoi@1pac.vn>
2018-01-23 10:20:12 +0700

    Add import csv action for admin holiday

--- app/admin/holidays.rb
+++ app/admin/holidays.rb
@@ -12,6 +12,25 @@ ActiveAdmin.register Holiday do
   filter :started_at
   filter :ended_at
 
+  collection_action :import_csv, method: :post do
+    return redirect_to import_csv_form_admin_holidays_path, flash: { error: 'CSV invalid file!' } unless params[:file]
+
+    c_params = { country: params[:file].original_filename.split('.')[0], admin_id: current_admin.id }
+    holidays = []
+    CSV.foreach(params[:file].tempfile, headers: true) do |row|
+      holidays << row.to_hash.merge(c_params)
+    end
+    Holiday.import holidays
+    redirect_to admin_holidays_path, notice: 'CSV imported successfully!'
+  end
+
+  collection_action :import_csv_form, method: :get do
+  end
+
+  action_item :view, only: :index do
+    link_to 'Import Csv', import_csv_form_admin_holidays_path
+  end
+
   index do
     selectable_column
     column :name
