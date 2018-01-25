a39fa4c2cd4abd6c64d0cfcf4404300a529ab226
Minh Phan <wofi.minh@1pac.vn>
2018-01-25 14:07:46 +0700

    Fix and implement new pages

--- frontend/src/pages/GroupAttendances.vue
+++ frontend/src/pages/GroupAttendances.vue
@@ -6,7 +6,38 @@
       <router-link tag="li" class="tab-item" to="/attendances/groups"><a href="#">Group Attendances</a></router-link>
     </ul>
 
-    <table class="table table-hover mt-4">
+    <div class="toolbar mt-5">
+      <datepicker
+        :placeholder="'From Date'"
+        :minimumView="'day'"
+        :maximumView="'day'"
+        :input-class="'datepicker-input form-input'"
+        :calendar-class="'datepicker-calendar'"
+        :wrapper-class="'datepicker'"/>
+      <datepicker
+        :placeholder="'To Date'"
+        :minimumView="'day'"
+        :maximumView="'day'"
+        :input-class="'datepicker-input form-input'"
+        :calendar-class="'datepicker-calendar'"
+        :wrapper-class="'datepicker'"/>
+      <select class="form-select">
+        <option>Filter by status</option>
+        <option>Attend on time</option>
+        <option>Leave on time</option>
+        <option>Holiday</option>
+        <option>Annual leave</option>
+      </select>
+      <select class="form-select">
+        <option>Filter by group</option>
+        <option>Marketing</option>
+        <option>Developers</option>
+        <option>HR</option>
+        <option>Accountant</option>
+      </select>
+    </div>
+
+    <table class="table table-hover bg-light mt-5">
       <thead>
         <th>Date</th>
         <th>Started At</th>
@@ -14,6 +45,51 @@
         <th>Status</th>
       </thead>
       <tbody>
+        <tr>
+          <td>Jan 20</td>
+          <td>08:30</td>
+          <td>18:00</td>
+          <td>
+            <span class="label label-rounded label-secondary">Attend on time</span>
+            <span class="label label-rounded label-success">Leave on time</span>
+          </td>
+        </tr>
+        <tr>
+          <td>Jan 20</td>
+          <td>08:30</td>
+          <td>18:00</td>
+          <td>
+            <span class="label label-rounded label-secondary">Attend on time</span>
+            <span class="label label-rounded label-success">Leave on time</span>
+          </td>
+        </tr>
+        <tr>
+          <td>Jan 20</td>
+          <td>08:30</td>
+          <td>18:00</td>
+          <td>
+            <span class="label label-rounded label-secondary">Attend on time</span>
+            <span class="label label-rounded label-success">Leave on time</span>
+          </td>
+        </tr>
+        <tr>
+          <td>Jan 20</td>
+          <td>08:30</td>
+          <td>18:00</td>
+          <td>
+            <span class="label label-rounded label-secondary">Attend on time</span>
+            <span class="label label-rounded label-success">Leave on time</span>
+          </td>
+        </tr>
+        <tr>
+          <td>Jan 20</td>
+          <td>08:30</td>
+          <td>18:00</td>
+          <td>
+            <span class="label label-rounded label-secondary">Attend on time</span>
+            <span class="label label-rounded label-success">Leave on time</span>
+          </td>
+        </tr>
         <tr>
           <td>Jan 20</td>
           <td>08:30</td>
@@ -89,10 +165,12 @@
 </template>
 
 <script>
+import Datepicker from 'vuejs-datepicker'
 import MainLayout from '../layouts/Main.vue'
 
 export default {
   components: {
+    Datepicker,
     MainLayout
   }
 }
---
+++ frontend/src/pages/GroupRequests.vue
@@ -0,0 +1,107 @@
+<template>
+  <main-layout>
+    <h1>Requests</h1>
+    <ul class="tab mt-4">
+      <router-link tag="li" class="tab-item" to="/requests/my"><a href="#">My Requests</a></router-link>
+      <router-link tag="li" class="tab-item" to="/requests/groups"><a href="#">Group Requests</a></router-link>
+    </ul>
+
+    <div class="toolbar mt-5">
+      <select class="form-select">
+        <option>Choose a group</option>
+        <option>HR</option>
+        <option>Accountant</option>
+        <option>Engineer</option>
+        <option>Administrator</option>
+      </select>
+      <select class="form-select">
+        <option>Filter by status</option>
+        <option>Approved</option>
+        <option>Pending</option>
+        <option>Denied</option>
+      </select>
+    </div>
+
+    <table class="table table-hover bg-light mt-4">
+      <thead>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Date</th>
+        <th>Started At</th>
+        <th>Ended At</th>
+        <th>Reason</th>
+        <th>Status</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Minh</td>
+          <td>wofi.minh@1pac.vn</td>
+          <td>Jan 20</td>
+          <td>08:30</td>
+          <td>18:00</td>
+          <td>I have personal issue, so I will be late for 10mins</td>
+          <td>
+            <span class="label label-rounded label-success">Approved</span>
+          </td>
+        </tr>
+        <tr>
+          <td>Minh</td>
+          <td>wofi.minh@1pac.vn</td>
+          <td>Jan 20</td>
+          <td>08:30</td>
+          <td>18:00</td>
+          <td>I have personal issue, so I will be late for 10mins</td>
+          <td>
+            <span class="label label-rounded label-warning">Pending</span>
+          </td>
+        </tr>
+        <tr>
+          <td>Minh</td>
+          <td>wofi.minh@1pac.vn</td>
+          <td>Jan 20</td>
+          <td>08:30</td>
+          <td>18:00</td>
+          <td>I have personal issue, so I will be late for 10mins</td>
+          <td>
+            <span class="label label-rounded label-error">Rejected</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <ul class="pagination mt-4">
+      <li class="page-item">
+        <a><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg></a>
+      </li>
+      <li class="page-item active">
+        <a href="#">1</a>
+      </li>
+      <li class="page-item">
+        <a href="#">2</a>
+      </li>
+      <li class="page-item">
+        <a href="#">3</a>
+      </li>
+      <li class="page-item">
+        <span>...</span>
+      </li>
+      <li class="page-item">
+        <a href="#">12</a>
+      </li>
+      <li class="page-item">
+        <a><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z"/></svg></a>
+      </li>
+    </ul>
+  </main-layout>
+</template>
+
+<script>
+import MainLayout from '../layouts/Main.vue'
+
+export default {
+  name: 'group-requests',
+  components: {
+    MainLayout
+  }
+}
+</script>
