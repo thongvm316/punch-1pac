4cbdea58336e860c688adf57cfbe69416c50039c
DiamondDat <diamond.dat@1pac.vn>
2018-01-25 17:25:36 +0700

    Fix company settings code as feedback

--- frontend/src/pages/CompanySettingsAllowedIPs.vue
+++ frontend/src/pages/CompanySettingsAllowedIPs.vue
@@ -7,38 +7,14 @@
       </div>
       <div class="column col-9">
         <h2 class="border-bottom">Allowed IPs</h2>
-        <div class="toolbar mt-5">
-          <div class="form-group clearfix">
-            <select class="form-select">
-              <option>Filter by name</option>
-            </select>
-            <a href="#add-ip" class="btn ml-5">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
-              Add IP
-            </a>
-          </div>
-          <div class="modal" id="add-ip">
-            <a href="#close" class="modal-overlay"></a>
-            <div class="modal-container">
-              <div class="modal-header">
-                <a href="#close" class="btn btn-clear float-right"></a><br>
-                <h2 class="text-center">Add IP</h2>
-              </div>
-              <div class="modal-body">
-                <div class="content">
-                  <div class="form-group">
-                    <label class="form-label">IP Address</label>
-                    <input class="form-input" type="text">
-                  </div>
-                </div>
-              </div>
-              <div class="modal-footer">
-                <div class="text-center">
-                  <a href="#close" class="btn btn-primary">Submit</a>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div class="toolbar clearfix mt-5">
+          <select class="form-select">
+            <option>Filter by name</option>
+          </select>
+          <a href="#add-ip" class="btn float-right">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
+            Add IP
+          </a>
         </div>
         <table class="table table-hover bg-light mt-5">
           <thead>
@@ -76,6 +52,31 @@
         </table>
       </div>
     </div>
+    <!-- Add IP modal -->
+    <div class="modal" id="add-ip">
+      <a href="#close" class="modal-overlay"></a>
+      <div class="modal-container">
+        <div class="modal-header">
+          <a href="#close" class="btn btn-clear float-right"></a><br>
+          <h2 class="text-center">Add IP</h2>
+        </div>
+        <div class="modal-body">
+          <div class="content">
+            <div class="form-group">
+              <label class="form-label">IP Address</label>
+              <input class="form-input" type="text">
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <div class="text-center">
+            <a href="#close" class="btn btn-primary">Submit</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Edit IP modal -->
     <div class="modal" :class="{ active: isModalOpen }" id="edit-ip">
       <a class="modal-overlay" @click="toggleModal"></a>
       <div class="modal-container">
--- frontend/src/pages/CompanySettingsHolidays.vue
+++ frontend/src/pages/CompanySettingsHolidays.vue
@@ -8,61 +8,27 @@
       <div class="column col-9">
         <h2 class="border-bottom">Holidays</h2>
         <div class="toolbar mt-5">
-          <div class="form-group">
-            <select class="form-select">
-              <option>Select country to import</option>
-              <option>Vietnam</option>
-              <option>Korea</option>
-              <option>Quatar</option>
-              <option>Uzbekistan</option>
-            </select>
-            <button class="btn btn-primary ml-5">Import</button>
-          </div>
+          <select class="form-select">
+            <option>Select country to import</option>
+            <option>Vietnam</option>
+            <option>Korea</option>
+            <option>Quatar</option>
+            <option>Uzbekistan</option>
+          </select>
+          <button class="btn btn-primary ml-5">Import</button>
         </div>
-        <div class="toolbar mt-5">
-          <div class="form-group clearfix">
-            <select class="form-select">
-              <option>Filter holiday by name</option>
-              <option>Vietnam</option>
-              <option>Korea</option>
-              <option>Quatar</option>
-              <option>Uzbekistan</option>
-            </select>
-            <a href="#add-holiday" class="btn float-right">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
-              Add Holiday
-            </a>
-          </div>
-          <div class="modal" id="add-holiday">
-            <a href="#close" class="modal-overlay"></a>
-            <div class="modal-container">
-              <div class="modal-header">
-                <a href="#close" class="btn btn-clear float-right"></a><br>
-                <h2 class="text-center">Add Holiday</h2>
-              </div>
-              <div class="modal-body">
-                <div class="content">
-                  <div class="form-group">
-                    <label class="form-label">Name</label>
-                    <input class="form-input" type="text">
-                  </div>
-                  <div class="form-group">
-                    <label class="form-label">Start at</label>
-                    <input class="form-input" type="text">
-                  </div>
-                  <div class="form-group">
-                    <label class="form-label">End at</label>
-                    <input class="form-input" type="text">
-                  </div>
-                </div>
-              </div>
-              <div class="modal-footer">
-                <div class="text-center">
-                  <a href="#close" class="btn btn-primary">Submit</a>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div class="toolbar clearfix mt-5">
+          <select class="form-select">
+            <option>Filter holiday by name</option>
+            <option>Vietnam</option>
+            <option>Korea</option>
+            <option>Quatar</option>
+            <option>Uzbekistan</option>
+          </select>
+          <a href="#add-holiday" class="btn float-right">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
+            Add Holiday
+          </a>
         </div>
 
         <table class="table table-hover bg-light mt-5">
@@ -83,6 +49,37 @@
             </tr>
           </tbody>
         </table>
+      </div>
+    </div>
+    <!-- Add holiday -->
+    <div class="modal" id="add-holiday">
+      <a href="#close" class="modal-overlay"></a>
+      <div class="modal-container">
+        <div class="modal-header">
+          <a href="#close" class="btn btn-clear float-right"></a><br>
+          <h2 class="text-center">Add Holiday</h2>
+        </div>
+        <div class="modal-body">
+          <div class="content">
+            <div class="form-group">
+              <label class="form-label">Name</label>
+              <input class="form-input" type="text">
+            </div>
+            <div class="form-group">
+              <label class="form-label">Start at</label>
+              <input class="form-input" type="text">
+            </div>
+            <div class="form-group">
+              <label class="form-label">End at</label>
+              <input class="form-input" type="text">
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <div class="text-center">
+            <a href="#close" class="btn btn-primary">Submit</a>
+          </div>
+        </div>
       </div>
     </div>
     <div class="modal" :class="{ active: isModalOpen }" id="edit-holiday">
