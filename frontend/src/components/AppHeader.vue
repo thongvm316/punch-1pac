83abad3a17882c7385e1143d720facb8f2645595
DiamondDat <diamond.dat@1pac.vn>
2018-01-26 10:14:58 +0700

    Make noti popup

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -14,12 +14,75 @@
         <section class="navbar-section">
           <span class="text-bold-600 mr-5">Jan 22, 2018 {{ currentTime }}</span>
           <button class="btn btn-primary mr-5">Punch In</button>
-          <span class="announcement mr-5" :class="{ badge: num_announcement }" :data-badge="num_announcement">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path
-              d="M15.137 3.945c-.644-.374-1.042-1.07-1.041-1.82v-.003c.001-1.172-.938-2.122-2.096-2.122s-2.097.95-2.097
-              2.122v.003c.001.751-.396 1.446-1.041 1.82-4.667 2.712-1.985 11.715-6.862 13.306v1.749h20v-1.749c-4.877-1.591-2.195-10.594-6.863-13.306zm-3.137-2.945c.552
-              0 1 .449 1 1 0 .552-.448 1-1 1s-1-.448-1-1c0-.551.448-1 1-1zm3 20c0 1.598-1.392 3-2.971 3s-3.029-1.402-3.029-3h6z"/></svg>
-          </span>
+          <div class="dropdown mr-5" :class="{ active: isNotiActive }" @click="toggleNoti">
+            <span class="announcement" :class="{ badge: num_announcement }" :data-badge="num_announcement">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path
+                d="M15.137 3.945c-.644-.374-1.042-1.07-1.041-1.82v-.003c.001-1.172-.938-2.122-2.096-2.122s-2.097.95-2.097
+                2.122v.003c.001.751-.396 1.446-1.041 1.82-4.667 2.712-1.985 11.715-6.862 13.306v1.749h20v-1.749c-4.877-1.591-2.195-10.594-6.863-13.306zm-3.137-2.945c.552
+                0 1 .449 1 1 0 .552-.448 1-1 1s-1-.448-1-1c0-.551.448-1 1-1zm3 20c0 1.598-1.392 3-2.971 3s-3.029-1.402-3.029-3h6z"/></svg>
+            </span>
+            <div class="panel menu">
+              <div class="panel-header">
+                <div class="panel-title h4">Announcements</div>
+              </div>
+              <div class="panel-body" v-show="temp_num === 0">
+                <div class="announcement-item">
+                  <p>NOTHING HERE!</p>
+                </div>
+              </div>
+              <div class="panel-body" v-show="temp_num > 0">
+                <ul class="announcement-items">
+                  <router-link to="/announcements/1" class="announcement-item" tag="li">
+                    <span>2018-01-26</span>
+                    <p class="text-bold">Title 1</p>
+                  </router-link>
+                  <router-link to="/announcements/1" class="announcement-item" tag="li">
+                    <span>2018-01-26</span>
+                    <p class="text-bold">Title 1</p>
+                  </router-link>
+                  <router-link to="/announcements/1" class="announcement-item" tag="li">
+                    <span>2018-01-26</span>
+                    <p class="text-bold">Title 1</p>
+                  </router-link>
+                  <router-link to="/announcements/1" class="announcement-item" tag="li">
+                    <span>2018-01-26</span>
+                    <p class="text-bold">Title 1</p>
+                  </router-link>
+                  <router-link to="/announcements/1" class="announcement-item" tag="li">
+                    <span>2018-01-26</span>
+                    <p class="text-bold">Title 1</p>
+                  </router-link>
+                  <router-link to="/announcements/1" class="announcement-item" tag="li">
+                    <span>2018-01-26</span>
+                    <p class="text-bold">Title 1</p>
+                  </router-link>
+                  <router-link to="/announcements/1" class="announcement-item" tag="li">
+                    <span>2018-01-26</span>
+                    <p class="text-bold">Title 1</p>
+                  </router-link>
+                  <router-link to="/announcements/1" class="announcement-item" tag="li">
+                    <span>2018-01-26</span>
+                    <p class="text-bold">Title 1</p>
+                  </router-link>
+                  <router-link to="/announcements/1" class="announcement-item" tag="li">
+                    <span>2018-01-26</span>
+                    <p class="text-bold">Title 1</p>
+                  </router-link>
+                  <router-link to="/announcements/1" class="announcement-item" tag="li">
+                    <span>2018-01-26</span>
+                    <p class="text-bold">Title 1</p>
+                  </router-link>
+                  <router-link to="/announcements/1" class="announcement-item" tag="li">
+                    <span>2018-01-26</span>
+                    <p class="text-bold">Title 1</p>
+                  </router-link>
+                </ul>
+              </div>
+              <div class="panel-footer text-center" v-show="temp_num > 0">
+                <router-link to="/announcements" tag="a">See All</router-link>
+              </div>
+            </div>
+          </div>
           <div class="dropdown" :class="{ active: isDropdownActive }" @click="toggleDropdown">
             <a class="dropdown-toggle">
               <div class="tile tile-centered">
@@ -50,8 +113,10 @@ export default {
   data () {
     return {
       isDropdownActive: false,
-      num_announcement: 0,
-      currentTime: null
+      currentTime: null,
+      isNotiActive: false,
+      num_announcement: 3,
+      temp_num: 0
     }
   },
 
@@ -67,6 +132,12 @@ export default {
 
     updateCurrentTime () {
       this.currentTime = moment().format('HH:mm')
+    },
+
+    toggleNoti () {
+      this.isNotiActive = !this.isNotiActive
+      this.temp_num = this.num_announcement
+      this.num_announcement = 0
     }
   },
 
