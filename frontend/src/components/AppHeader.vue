bc69953dfb5c7bbe7ea3a445d333bfe31b51eb8c
Minh Phan <wofi.minh@1pac.vn>
2018-01-29 16:08:47 +0700

    Restyle announcement dropdown

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -12,75 +12,60 @@
           <router-link class="mr-5" to="/groups">Groups</router-link>
         </section>
         <section class="navbar-section">
-          <span class="text-bold-600 mr-5">Jan 22, 2018 {{ currentTime }}</span>
+          <span class="text-bold-600 mr-5">{{ currentTime }}</span>
           <button class="btn btn-primary mr-5">Punch In</button>
           <div class="dropdown mr-5" :class="{ active: isNotiActive }" @click="toggleNoti">
-            <span class="announcement" :class="{ badge: num_announcement }" :data-badge="num_announcement">
+            <span class="announcement" :class="{ badge: numAnnouncement }" :data-badge="numAnnouncement">
               <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path
                 d="M15.137 3.945c-.644-.374-1.042-1.07-1.041-1.82v-.003c.001-1.172-.938-2.122-2.096-2.122s-2.097.95-2.097
                 2.122v.003c.001.751-.396 1.446-1.041 1.82-4.667 2.712-1.985 11.715-6.862 13.306v1.749h20v-1.749c-4.877-1.591-2.195-10.594-6.863-13.306zm-3.137-2.945c.552
                 0 1 .449 1 1 0 .552-.448 1-1 1s-1-.448-1-1c0-.551.448-1 1-1zm3 20c0 1.598-1.392 3-2.971 3s-3.029-1.402-3.029-3h6z"/></svg>
             </span>
-            <div class="panel menu">
-              <div class="panel-header">
-                <div class="panel-title h4">Announcements</div>
-              </div>
-              <div class="panel-body" v-show="temp_num === 0">
-                <div class="announcement-item">
-                  <p>NOTHING HERE!</p>
-                </div>
-              </div>
-              <div class="panel-body" v-show="temp_num > 0">
-                <ul class="announcement-items">
-                  <router-link to="/announcements/1" class="announcement-item" tag="li">
-                    <span>2018-01-26</span>
-                    <p class="text-bold">Title 1</p>
-                  </router-link>
-                  <router-link to="/announcements/1" class="announcement-item" tag="li">
-                    <span>2018-01-26</span>
-                    <p class="text-bold">Title 1</p>
-                  </router-link>
-                  <router-link to="/announcements/1" class="announcement-item" tag="li">
-                    <span>2018-01-26</span>
-                    <p class="text-bold">Title 1</p>
-                  </router-link>
-                  <router-link to="/announcements/1" class="announcement-item" tag="li">
-                    <span>2018-01-26</span>
-                    <p class="text-bold">Title 1</p>
-                  </router-link>
-                  <router-link to="/announcements/1" class="announcement-item" tag="li">
-                    <span>2018-01-26</span>
-                    <p class="text-bold">Title 1</p>
-                  </router-link>
-                  <router-link to="/announcements/1" class="announcement-item" tag="li">
-                    <span>2018-01-26</span>
-                    <p class="text-bold">Title 1</p>
-                  </router-link>
-                  <router-link to="/announcements/1" class="announcement-item" tag="li">
-                    <span>2018-01-26</span>
-                    <p class="text-bold">Title 1</p>
-                  </router-link>
-                  <router-link to="/announcements/1" class="announcement-item" tag="li">
-                    <span>2018-01-26</span>
-                    <p class="text-bold">Title 1</p>
-                  </router-link>
-                  <router-link to="/announcements/1" class="announcement-item" tag="li">
-                    <span>2018-01-26</span>
-                    <p class="text-bold">Title 1</p>
-                  </router-link>
-                  <router-link to="/announcements/1" class="announcement-item" tag="li">
-                    <span>2018-01-26</span>
-                    <p class="text-bold">Title 1</p>
-                  </router-link>
-                  <router-link to="/announcements/1" class="announcement-item" tag="li">
-                    <span>2018-01-26</span>
-                    <p class="text-bold">Title 1</p>
-                  </router-link>
-                </ul>
-              </div>
-              <div class="panel-footer text-center" v-show="temp_num > 0">
-                <router-link to="/announcements" tag="a">See All</router-link>
-              </div>
+            <div class="box announcement-dropdown">
+              <h4>Announcements</h4>
+              <ul>
+                <router-link to="/announcements/1" tag="li">
+                  <p>Title 1</p>
+                  <span>2018-01-26</span>
+                </router-link>
+                <router-link to="/announcements/1" tag="li">
+                  <p>Title 1</p>
+                  <span>2018-01-26</span>
+                </router-link>
+                <router-link to="/announcements/1" tag="li">
+                  <p>Title 1</p>
+                  <span>2018-01-26</span>
+                </router-link>
+                <router-link to="/announcements/1" tag="li">
+                  <p>Title 1</p>
+                  <span>2018-01-26</span>
+                </router-link>
+                <router-link to="/announcements/1" tag="li">
+                  <p>Title 1</p>
+                  <span>2018-01-26</span>
+                </router-link>
+                <router-link to="/announcements/1" tag="li">
+                  <p>Title 1</p>
+                  <span>2018-01-26</span>
+                </router-link>
+                <router-link to="/announcements/1" tag="li">
+                  <p>Title 1</p>
+                  <span>2018-01-26</span>
+                </router-link>
+                <router-link to="/announcements/1" tag="li">
+                  <p>Title 1</p>
+                  <span>2018-01-26</span>
+                </router-link>
+                <router-link to="/announcements/1" tag="li">
+                  <p>Title 1</p>
+                  <span>2018-01-26</span>
+                </router-link>
+                <router-link to="/announcements/1" tag="li">
+                  <p>Title 1</p>
+                  <span>2018-01-26</span>
+                </router-link>
+              </ul>
+              <router-link to="/announcements" tag="a">See All</router-link>
             </div>
           </div>
           <div class="dropdown" :class="{ active: isDropdownActive }" @click="toggleDropdown">
@@ -115,8 +100,7 @@ export default {
       isDropdownActive: false,
       currentTime: null,
       isNotiActive: false,
-      num_announcement: 3,
-      temp_num: 0
+      numAnnouncement: 3
     }
   },
 
@@ -131,13 +115,11 @@ export default {
     },
 
     updateCurrentTime () {
-      this.currentTime = moment().format('HH:mm')
+      this.currentTime = moment().format('HH:mm:ss')
     },
 
     toggleNoti () {
       this.isNotiActive = !this.isNotiActive
-      this.temp_num = this.num_announcement
-      this.num_announcement = 0
     }
   },
 
@@ -160,8 +142,8 @@ export default {
   },
 
   created () {
-    this.currentTime = moment().format('HH:mm')
-    setInterval(() => this.updateCurrentTime(), 1 * 1000)
+    this.currentTime = moment().format('HH:mm:ss')
+    setInterval(this.updateCurrentTime, 1 * 1000)
   }
 }
 </script>
