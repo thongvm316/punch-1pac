b33616973f7efc50395f87c844a101632c6a69ef
DiamondDat <diamond.dat@1pac.vn>
2018-01-29 17:41:34 +0700

    Add vue-i18n

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,39 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueI18n from 'vue-i18n'
 
 // import axios from 'axios'
 
 import './scss/main.scss'
+
+Vue.use(VueI18n)
+
+const messages = {
+  en: {
+    message: {
+      hello: 'Hello World!',
+      goodbye: 'Good bye!'
+    }
+  },
+  ja: {
+    message: {
+      hello: 'こんにちは、世界',
+      goodbye: 'さよなら'
+    }
+  },
+  vi: {
+    message: {
+      hello: 'Xin chào thế giới!',
+      goodbye: 'Tạm biệt!'
+    }
+  }
+}
+
+const i18n = new VueI18n({
+  locale: 'ja',
+  messages
+})
 
 // axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name=csrf-token]').getAttribute('content')
 
@@ -17,5 +46,6 @@ router.beforeEach((to, from, next) => {
 new Vue({
   el: '#app',
   router,
+  i18n,
   render: h => h(App)
 })
