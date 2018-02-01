f85b32ef75cd08bc0404fc5e22a9a62359f7cb86
Minh Phan <wofi.minh@1pac.vn>
2018-02-01 08:39:37 +0700

    Change api base url

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,9 @@ import App from './App'
 import router from './router'
 import VueI18n from 'vue-i18n'
 import messages from './locale/messages'
+import store from './store'
 
-// import axios from 'axios'
+import axios from 'axios'
 
 import './scss/main.scss'
 
@@ -17,6 +18,9 @@ const i18n = new VueI18n({
 })
 
 // axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name=csrf-token]').getAttribute('content')
+axios.defaults.baseURL = 'http://namespace_1.localhost:3000/api/v1'
+axios.defaults.headers.common['Authorization'] = 'Bear eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MTk4OTUzOTgsImp0aSI6ImJmNmYyMDY1LTNhZTAtNGJhYS1iODhlLWI0YjUzMTc4YTQ1MyIsInN1YiI6MX0.2oPg2ApRG-fpVnMTDz3qCDVj-S45W6wCt-j9EO0ZVvo'
+axios.defaults.headers.common['Accept'] = 'application/json'
 
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title
@@ -28,5 +32,6 @@ new Vue({
   el: '#app',
   router,
   i18n,
+  store,
   render: h => h(App)
 })
