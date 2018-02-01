81bdbdd02d872256e1c0c3bc75931f19f001678f
Minh Phan <wofi.minh@1pac.vn>
2018-02-01 08:39:19 +0700

    Setup vuex

---
+++ frontend/src/store/modules/header-announcements.js
@@ -0,0 +1,29 @@
+import * as types from '../mutation-types'
+import axios from 'axios'
+
+const state = {
+  numAnnouncement: 3,
+  announcements: []
+}
+
+const mutations = {
+  [types.RECEIVE_ANNOUNCEMENTS] (state, payload) {
+    state.announcements = payload.announcements
+  }
+}
+
+const actions = {
+  getAnnouncements ({ commit }) {
+    axios.get('/announcements', { params: { per_page: 5 } })
+         .then((response) => {
+           commit(types.RECEIVE_ANNOUNCEMENTS, response.data)
+         })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
