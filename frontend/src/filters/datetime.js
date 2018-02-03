fe60f299ac014ce860b1062be8b36e473ff8e653
Minh Phan <wofi.minh@1pac.vn>
2018-02-03 15:12:53 +0700

    Add new date filter

--- frontend/src/filters/datetime.js
+++ frontend/src/filters/datetime.js
@@ -3,3 +3,4 @@ import moment from 'moment'
 
 Vue.filter('datetime_short', val => moment(val).format('MMM D, hh:mm'))
 Vue.filter('datetime_normal', val => moment(val).format('MMM DD YYYY, HH:mm'))
+Vue.filter('datetime_mmdd', val => moment(val).format('MMM DD'))
