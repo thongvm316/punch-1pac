5d9d892936c2d64e77eda6f4e3c39c5549c6cbce
Minh Phan <wofi.minh@1pac.vn>
2018-02-03 15:10:36 +0700

    Fix pagination meta response

--- app/controllers/concerns/pagination.rb
+++ app/controllers/concerns/pagination.rb
@@ -4,11 +4,12 @@ module Pagination
   extend ActiveSupport::Concern
 
   def pager(object)
+    per_page = params[:per_page].to_i
     {
       current_page: object.current_page,
       next_page: object.next_page,
       prev_page: object.prev_page,
-      per_page: params[:per_page].to_i || Kaminari.config.default_per_page,
+      per_page: per_page > 0 ? per_page : Kaminari.config.default_per_page,
       total_pages: object.total_pages
     }
   end
