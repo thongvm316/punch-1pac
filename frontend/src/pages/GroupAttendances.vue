fd262cfdb703a79da7d280dac854522ac91b1b32
Minh Phan <wofi.minh@1pac.vn>
2018-02-05 14:38:10 +0700

    Implement group attendance page

--- frontend/src/pages/GroupAttendances.vue
+++ frontend/src/pages/GroupAttendances.vue
@@ -8,158 +8,65 @@
     <div class="toolbar mt-5">
       <datepicker
         :placeholder="$t('placeholder.fromDate')"
+        :format="'MMM dd yyyy'"
         :minimumView="'day'"
         :maximumView="'day'"
         :input-class="'datepicker-input form-input'"
         :calendar-class="'datepicker-calendar'"
-        :wrapper-class="'datepicker'"/>
+        :wrapper-class="'datepicker'"
+        v-model="params.from_date"/>
       <datepicker
         :placeholder="$t('placeholder.toDate')"
+        :format="'MMM dd yyyy'"
         :minimumView="'day'"
         :maximumView="'day'"
         :input-class="'datepicker-input form-input'"
         :calendar-class="'datepicker-calendar'"
-        :wrapper-class="'datepicker'"/>
-      <select class="form-select">
-        <option>{{ $t('placeholder.filterByStatus') }}</option>
-        <option>{{ $t('status.arriveOnTime') }}</option>
-        <option>{{ $t('status.leaveOnTime') }}</option>
-        <option>{{ $t('status.arriveLate') }}</option>
-        <option>{{ $t('status.leaveEarly') }}</option>
-        <option>{{ $t('status.holiday') }}</option>
-        <option>{{ $t('status.annualLeave') }}</option>
+        :wrapper-class="'datepicker'"
+        v-model="params.to_date"/>
+      <select class="form-select" v-model="params.status">
+        <option value="">{{ $t('placeholder.filterByStatus') }}</option>
+        <option value="attend_ok">{{ $t('status.arriveOnTime') }}</option>
+        <option value="leave_ok">{{ $t('status.leaveOnTime') }}</option>
+        <option value="attend_late">{{ $t('status.arriveLate') }}</option>
+        <option value="leave_early">{{ $t('status.leaveEarly') }}</option>
+        <option value="holiday">{{ $t('status.holiday') }}</option>
+        <option value="annual_leave">{{ $t('status.annualLeave') }}</option>
       </select>
-      <select class="form-select">
-        <option>{{ $t('placeholder.filterByGroup') }}</option>
-        <option>{{ $t('group.accountant') }}</option>
-        <option>{{ $t('group.developers') }}</option>
-        <option>{{ $t('group.marketing') }}</option>
-        <option>{{ $t('group.hr') }}</option>
+      <select class="form-select" v-model="params.group_id">
+        <option value="">{{ $t('placeholder.filterByGroup') }}</option>
+        <option value="1">{{ $t('group.accountant') }}</option>
+        <option value="2">{{ $t('group.developers') }}</option>
+        <option value="3">{{ $t('group.marketing') }}</option>
+        <option value="5">{{ $t('group.hr') }}</option>
       </select>
     </div>
 
     <table class="table table-hover bg-light mt-5">
       <thead>
+        <th>{{ $t('tableHeader.name') }}</th>
+        <th>{{ $t('tableHeader.email') }}</th>
         <th>{{ $t('tableHeader.date') }}</th>
         <th>{{ $t('tableHeader.startedAt') }}</th>
         <th>{{ $t('tableHeader.endedAt') }}</th>
         <th>{{ $t('tableHeader.status') }}</th>
       </thead>
       <tbody>
-        <tr>
-          <td>Minh</td>
-          <td>wofi.minh@1pac.vn</td>
-          <td>Jan 20</td>
-          <td>08:30</td>
-          <td>18:00</td>
+        <tr v-for="attendance in attendances">
+          <td>{{ attendance.user.name }}</td>
+          <td>{{ attendance.user.email }}</td>
+          <td>{{ attendance.day | datetime_mmdd }}</td>
+          <td>{{ attendance.attended_at }}</td>
+          <td>{{ attendance.left_at }}</td>
           <td>
-            <span class="label label-rounded label-secondary">{{ $t('status.arriveOnTime') }}</span>
-            <span class="label label-rounded label-success">{{ $t('status.leaveOnTime') }}</span>
-          </td>
-        </tr>
-        <tr>
-          <td>Minh</td>
-          <td>wofi.minh@1pac.vn</td>
-          <td>Jan 20</td>
-          <td>08:30</td>
-          <td>18:00</td>
-          <td>
-            <span class="label label-rounded label-secondary">{{ $t('status.arriveOnTime') }}</span>
-            <span class="label label-rounded label-success">{{ $t('status.leaveOnTime') }}</span>
-          </td>
-        </tr>
-        <tr>
-          <td>Minh</td>
-          <td>wofi.minh@1pac.vn</td>
-          <td>Jan 20</td>
-          <td>08:30</td>
-          <td>18:00</td>
-          <td>
-            <span class="label label-rounded label-secondary">{{ $t('status.arriveOnTime') }}</span>
-            <span class="label label-rounded label-success">{{ $t('status.leaveOnTime') }}</span>
-          </td>
-        </tr>
-        <tr>
-          <td>Minh</td>
-          <td>wofi.minh@1pac.vn</td>
-          <td>Jan 20</td>
-          <td>08:30</td>
-          <td>18:00</td>
-          <td>
-            <span class="label label-rounded label-secondary">{{ $t('status.arriveOnTime') }}</span>
-            <span class="label label-rounded label-success">{{ $t('status.leaveOnTime') }}</span>
-          </td>
-        </tr>
-        <tr>
-          <td>Minh</td>
-          <td>wofi.minh@1pac.vn</td>
-          <td>Jan 20</td>
-          <td>08:30</td>
-          <td>18:00</td>
-          <td>
-            <span class="label label-rounded label-secondary">{{ $t('status.arriveOnTime') }}</span>
-            <span class="label label-rounded label-success">{{ $t('status.leaveOnTime') }}</span>
-          </td>
-        </tr>
-        <tr>
-          <td>Minh</td>
-          <td>wofi.minh@1pac.vn</td>
-          <td>Jan 20</td>
-          <td>08:30</td>
-          <td>18:00</td>
-          <td>
-            <span class="label label-rounded label-secondary">{{ $t('status.arriveOnTime') }}</span>
-            <span class="label label-rounded label-success">{{ $t('status.leaveOnTime') }}</span>
-          </td>
-        </tr>
-        <tr>
-          <td>Minh</td>
-          <td>wofi.minh@1pac.vn</td>
-          <td>Jan 20</td>
-          <td>08:30</td>
-          <td>18:00</td>
-          <td>
-            <span class="label label-rounded label-secondary">{{ $t('status.arriveOnTime') }}</span>
-            <span class="label label-rounded label-success">{{ $t('status.leaveOnTime') }}</span>
-          </td>
-        </tr>
-        <tr>
-          <td>Minh</td>
-          <td>wofi.minh@1pac.vn</td>
-          <td>Jan 20</td>
-          <td>08:30</td>
-          <td>18:00</td>
-          <td>
-            <span class="label label-rounded label-secondary">{{ $t('status.arriveOnTime') }}</span>
-            <span class="label label-rounded label-success">{{ $t('status.leaveOnTime') }}</span>
-          </td>
-        </tr>
-        <tr>
-          <td>Minh</td>
-          <td>wofi.minh@1pac.vn</td>
-          <td>Jan 20</td>
-          <td>08:30</td>
-          <td>18:00</td>
-          <td>
-            <span class="label label-rounded label-secondary">{{ $t('status.arriveOnTime') }}</span>
-            <span class="label label-rounded label-success">{{ $t('status.leaveOnTime') }}</span>
-          </td>
-        </tr>
-        <tr>
-          <td>Minh</td>
-          <td>wofi.minh@1pac.vn</td>
-          <td>Jan 20</td>
-          <td>08:30</td>
-          <td>18:00</td>
-          <td>
-            <span class="label label-rounded label-secondary">{{ $t('status.arriveOnTime') }}</span>
-            <span class="label label-rounded label-success">{{ $t('status.leaveOnTime') }}</span>
+            <span class="label label-rounded label-secondary">{{ attendance.attending_status }}</span>
+            <span class="label label-rounded label-success">{{ attendance.leaving_status }}</span>
           </td>
         </tr>
       </tbody>
     </table>
 
-    <pagination/>
+    <pagination action="getAttendances" namespace="attendances" v-if="pager.total_pages > 1"/>
   </main-layout>
 </template>
 
@@ -167,12 +74,51 @@
 import Datepicker from 'vuejs-datepicker'
 import MainLayout from '../layouts/Main.vue'
 import Pagination from '../components/Pagination.vue'
+import moment from 'moment'
+import { mapState, mapActions } from 'vuex'
 
 export default {
+  data () {
+    return {
+      params: {
+        from_date: moment().startOf('month').format('MMM DD YYYY'),
+        to_date: moment().endOf('month').format('MMM DD YYYY'),
+        status: '',
+        group_id: ''
+      }
+    }
+  },
+
   components: {
     Datepicker,
     MainLayout,
     Pagination
+  },
+
+  computed: {
+    ...mapState('attendances', [
+      'pager',
+      'attendances'
+    ])
+  },
+
+  methods: {
+    ...mapActions('attendances', [
+      'getAttendances'
+    ])
+  },
+
+  created () {
+    this.getAttendances(this.params)
+  },
+
+  watch: {
+    params: {
+      handler: function () {
+        this.getAttendances(Object.assign({ page: 1 }, this.params))
+      },
+      deep: true
+    }
   }
 }
 </script>
