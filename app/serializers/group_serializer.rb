452ad8f9f3ea1946f22812bc60c8a0980f4902ff
Minh Phan <wofi.minh@1pac.vn>
2018-02-08 16:15:56 +0700

    Serialize group response

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 class GroupSerializer < ApplicationSerializer
-  attributes :id, :name
+  attributes :id, :name, :user_count
   has_many :users, serializer: UserSerializer
+  has_many :admins, serializer: UserSerializer, class_name: 'User', foreign_key: :user_id
 
   def users
     object.users.includes(:department)
+  end
+
+  def admins
+    object.users.admin
+  end
+
+  def user_count
+    object.users.count
   end
 end
