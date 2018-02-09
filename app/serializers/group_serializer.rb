f3f43c4edf3ccf262226067ee427ef7b8b18a736
Minh Phan <wofi.minh@1pac.vn>
2018-02-09 15:07:47 +0700

    Fix user and group response

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,19 +1,10 @@
 # frozen_string_literal: true
 
 class GroupSerializer < ApplicationSerializer
-  attributes :id, :name, :user_count
+  attributes :id, :name
   has_many :users, serializer: UserSerializer
-  has_many :admins, serializer: UserSerializer, class_name: 'User', foreign_key: :user_id
 
   def users
     object.users.includes(:department)
-  end
-
-  def admins
-    object.users.admin
-  end
-
-  def user_count
-    object.users.count
   end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :name, :avatar_url, :gender, :department_name
+  attributes :id, :email, :name, :avatar_url, :gender, :department_name, :role
 
   def department_name
     object.department&.name.to_s
