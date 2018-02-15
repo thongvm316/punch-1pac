cfc7db5d3982e626b918b6e6e7d2c0de3b361690
Minh Phan <wofi.minh@1pac.vn>
2018-02-15 23:57:33 +0700

    tmp

--- frontend/src/pages/UserSettingsProfile.vue
+++ frontend/src/pages/UserSettingsProfile.vue
@@ -2,31 +2,31 @@
   <setting-layout sidebar-type="user" title="Settings for Troy Kozey" subtitle="Profile">
     <form class="setting-form">
       <div class="form-group">
-        <label class="form-label" for="input-example-16">Avatar</label>
-        <img  class="img-profile" :src="userAvatarUrl" :alt="userName">
-        <input class="form-input" type="file" id="input-example-16">
+        <label class="form-label">Avatar</label>
+        <img class="img-profile" :src="userAvatarUrl" :alt="userName">
+        <input class="form-input" type="file" v-model="params.avatar">
       </div>
       <div class="form-group">
-        <label class="form-label" for="input-example-17">Name</label>
-        <input class="form-input" type="text" id="input-example-17" :value="userName" placeholder="Name">
+        <label class="form-label">Name</label>
+        <input class="form-input" type="text" :value="userName" placeholder="Name" v-model="params.name">
       </div>
       <div class="form-group">
-        <label class="form-label" for="input-example-18">Gender</label>
+        <label class="form-label">Gender</label>
         <label class="form-radio">
-          <input type="radio" name="gender" checked>
+          <input type="radio" v-model="params.gender">
           <i class="form-icon"></i> Male
         </label>
         <label class="form-radio">
-          <input type="radio" name="gender">
+          <input type="radio" v-model="params.gender">
           <i class="form-icon"></i> Female
         </label>
       </div>
       <div class="form-group">
-        <label class="form-label" for="input-example-21">Email</label>
-        <input class="form-input" type="text" id="input-example-21" :value="userEmail" placeholder="Email">
+        <label class="form-label">Email</label>
+        <input class="form-input" type="text" :value="userEmail" placeholder="Email" v-model="params.email">
       </div>
       <div class="form-group">
-        <button type="button" class="btn">Save</button>
+        <button type="button" class="btn" @click="updateUser(params)">Save</button>
       </div>
     </form>
   </setting-layout>
@@ -36,6 +36,17 @@
 import SettingLayout from '../layouts/Setting.vue'
 
 export default {
+  data () {
+    return {
+      params: {
+        avatar: null,
+        gender: null,
+        name: '',
+        email: ''
+      }
+    }
+  },
+
   components: {
     SettingLayout
   },
