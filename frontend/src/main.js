b1d0d9415c400225ebd334b1d1aa7e628c099be6
Minh Phan <wofi.minh@1pac.vn>
2018-03-05 18:25:34 +0900

    Update access token

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,7 +12,7 @@ import './scss/main.scss'
 
 // axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name=csrf-token]').getAttribute('content')
 axios.defaults.baseURL = 'http://namespace_1.localhost:3000/api/v1'
-axios.defaults.headers.common['Authorization'] = 'Bear eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjAxMjk1MTcsImp0aSI6IjE2NjM1MmI4LWJiZGQtNDcxZi05ZWJlLWY4NGRmNWUzMTYwMiIsInN1YiI6Mn0.h0cY5TECDMmfD0moAklDlDqsAPRePzJFa2sEV1eite4'
+axios.defaults.headers.common['Authorization'] = 'Bear eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjI4MDc4OTAsImp0aSI6ImMzNmM3YmNiLTA2MzAtNGE1Yy05YjUyLWY3MTUzY2E4ZGMyNyIsInN1YiI6Mn0.7-RIQ1ftgcUTAQr48muGWMP4Z6x9NHsSa50x0oRV5uw'
 axios.defaults.headers.common['Accept'] = 'application/json'
 
 router.beforeEach((to, from, next) => {
