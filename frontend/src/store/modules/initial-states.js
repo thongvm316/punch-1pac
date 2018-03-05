e8240643b78cb789d5cbba4e6b72f35e7b9d2935
Minh Phan <wofi.minh@1pac.vn>
2018-03-05 18:21:05 +0900

    Handler errors display at company setting pages

--- frontend/src/store/modules/initial-states.js
+++ frontend/src/store/modules/initial-states.js
@@ -1,7 +1,9 @@
 import * as types from '../mutation-types.js'
 import axios from 'axios'
 
-const states = {
+const state = {
+  userErrors: {},
+  companyErrors: {},
   currentUser: {},
   currentCompany: {}
 }
@@ -19,8 +21,24 @@ const mutations = {
     state.currentUser = payload
   },
 
+  [types.INITIAL_STATES_SET_USER_ERRORS] (state, payload) {
+    state.userErrors = payload.errors
+  },
+
+  [types.INITIAL_STATES_CLEAR_USER_ERRORS] (state, payload) {
+    state.userErrors = {}
+  },
+
   [types.INITIAL_STATES_UPDATE_COMPANY] (state, payload) {
     state.currentCompany = payload
+  },
+
+  [types.INITIAL_STATES_SET_COMPANY_ERRORS] (state, payload) {
+    state.companyErrors = payload.errors
+  },
+
+  [types.INITIAL_STATES_CLEAR_COMPANY_ERRORS] (state, payload) {
+    state.companyErrors = {}
   }
 }
 
@@ -35,36 +53,48 @@ const actions = {
 
   updateUser ({ commit }, data) {
     let formData = new FormData()
-    Object.keys(data.userParams).forEach(key => {
-      if (data.userParams[key]) formData.set(`user[${key}]`, data.userParams[key])
-    })
-    return new Promise((resolve, reject) => {
-      axios.put(`/users/${data.userId}`, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
-           .then(response => {
-             commit(types.INITIAL_STATES_UPDATE_USER, response.data)
-             resolve(response.data)
-           })
-    })
+    Object.keys(data.userParams).forEach(key => formData.set(`user[${key}]`, data.userParams[key] || ''))
+
+    axios.put(`/users/${data.userId}`, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+          .then(response => {
+            commit(types.INITIAL_STATES_UPDATE_USER, response.data)
+          })
+          .catch(error => {
+            if (error.response && error.response.status === 422) commit(types.INITIAL_STATES_SET_USER_ERRORS, error.response.data)
+          })
   },
 
   updateCompany ({ commit }, companyParams) {
     let formData = new FormData()
     Object.keys(companyParams).forEach(key => {
-      if (companyParams[key]) formData.set(`company[${key}]`, companyParams[key])
+      if (Array.isArray(companyParams[key])) {
+        companyParams[key].forEach(v => formData.append(`company[${key}]`, v || ''))
+      } else {
+        formData.set(`company[${key}]`, companyParams[key])
+      }
     })
-    return new Promise((resolve, reject) => {
-      axios.put('/company', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
-           .then(response => {
-             commit(types.INITIAL_STATES_UPDATE_COMPANY, response.data)
-             resolve(response.data)
-           })
-    })
+
+    axios.put('/company', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+          .then(response => {
+            commit(types.INITIAL_STATES_UPDATE_COMPANY, response.data)
+          })
+          .catch(error => {
+            if (error.response && error.response.status === 422) commit(types.INITIAL_STATES_SET_COMPANY_ERRORS, error.response.data)
+          })
+  },
+
+  clearUserErrors ({ commit }) {
+    commit(types.INITIAL_STATES_CLEAR_USER_ERRORS)
+  },
+
+  clearCompanyErrors ({ commit }) {
+    commit(types.INITIAL_STATES_CLEAR_COMPANY_ERRORS)
   }
 }
 
 export default {
   namespaced: true,
-  states,
+  state,
   mutations,
   actions
 }
