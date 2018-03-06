98760cd46b3cbc00c9e3fdd802e190fbe8098d5e
Minh Phan <wofi.minh@1pac.vn>
2018-03-06 18:25:25 +0900

    Test national_holiday model validations

--- spec/models/national_holiday_spec.rb
+++ spec/models/national_holiday_spec.rb
@@ -3,6 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe NationalHoliday, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:country) }
+    it { should validate_inclusion_of(:country).in_array(%w[vietnam japan]) }
+    it { should validate_presence_of(:started_at) }
+    it { should validate_presence_of(:ended_at) }
+  end
+
   describe 'associations' do
     it { should belong_to(:admin) }
   end
