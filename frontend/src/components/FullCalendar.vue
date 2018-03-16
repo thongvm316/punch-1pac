cf741ad12d6fcc6ee3450206562931cba435d723
Minh Phan <wofi.minh@1pac.vn>
2018-03-16 10:52:46 +0700

    Fix calendar render logic

--- frontend/src/components/FullCalendar.vue
+++ frontend/src/components/FullCalendar.vue
@@ -18,12 +18,13 @@
           <div class="calendar-date prev-month disabled" v-for="date in lastDaysPreviousMonth">
             <button class="date-item">{{ date }}</button>
           </div>
-          <div class="calendar-date current-month" v-for="attendance in getFormattedAttendances">
+          <div class="calendar-date current-month" v-for="attendance in attendances">
             <button class="date-item" :class="{ 'date-today': attendance.day === today.format('YYYY-MM-DD') }">{{ attendance.day.split('-')[2] }}</button>
             <div class="calendar-events">
               <a href="#" class="calendar-event bg-primary text-light" v-if="attendance.attending_status">{{ attendance.attending_status }}</a>
               <a href="#" class="calendar-event bg-error text-light" v-if="attendance.leaving_status">{{ attendance.leaving_status }}</a>
               <a href="#" class="calendar-event bg-warning text-light" v-if="attendance.off_status">{{ attendance.off_status }}</a>
+              <a href="#" class="calendar-event bg-success text-light" v-if="attendance.holiday">{{ attendance.holiday.name }}</a>
             </div>
           </div>
           <div class="calendar-date next-month disabled" v-for="date in firstDaysNextMonth">
@@ -36,7 +37,7 @@
 </template>
 
 <script>
-import { mapState, mapGetters, mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'full-calendar',
@@ -45,7 +46,8 @@ export default {
     return {
       today: this.$moment(),
       dateContext: this.$moment(),
-      days: this.$moment.weekdaysShort()
+      days: this.$moment.weekdaysShort(),
+      attendances: []
     }
   },
 
@@ -122,24 +124,65 @@ export default {
       return nextDays
     },
 
-    ...mapState('calendar', [
-      'attendances'
-    ]),
-
-    ...mapGetters('calendar', [
-      'getFormattedAttendances'
+    ...mapState('initialStates', [
+      'currentUser',
+      'currentCompany'
     ])
   },
 
   methods: {
     nextMonth () {
       this.dateContext = this.$moment(this.dateContext).add(1, 'month')
-      this.getCalendarAttendances(this.dateContext.format('YYYY-MM-DD'))
+      this.getCalendarAttendances(this.dateContext.format('YYYY-MM-DD')).then(response => this.formatAttendances(response))
     },
 
     lastMonth () {
       this.dateContext = this.$moment(this.dateContext).subtract(1, 'month')
-      this.getCalendarAttendances(this.dateContext.format('YYYY-MM-DD'))
+      this.getCalendarAttendances(this.dateContext.format('YYYY-MM-DD')).then(response => this.formatAttendances(response))
+    },
+
+    formatAttendances (response) {
+      this.attendances = []
+      let attendances = []
+      const date = response.data.attendances[0] ? this.$moment(response.data.attendances[0].day) : this.$moment()
+      const userJoinDate = this.$moment(this.currentUser.created_at)
+      const findHolidayByDay = function (currentDay) {
+        return response.data.holidays.find(holiday => {
+          return this.$moment(currentDay).isBetween(holiday.started_at, holiday.ended_at)
+        })
+      }
+
+      // Loop render day in month
+      for (let day = 1; day <= date.daysInMonth(); day++) {
+        const currentDay = this.$moment(`${date.year()}-${date.format('MM')}-${day}`, 'YYYY-MM-D')
+        let attendance = { id: null, day: currentDay.format('YYYY-MM-DD'), attended_at: '', left_at: '', attending_status: '', leaving_status: '', off_status: '', holiday: null }
+        let holiday = null
+
+        // Check if current rendering day is a holiday
+        if (response.data.holidays) holiday = findHolidayByDay.call(this, currentDay)
+
+        // If currentDay is a holiday, set holiday object into attendance object
+        if (holiday) attendance = Object.assign({}, attendance, { holiday: holiday })
+
+        // A valid day is a day before today and after current user's join date
+        if (this.$moment().isAfter(currentDay) && this.$moment(currentDay).isAfter(userJoinDate)) {
+          const tmpAttendance = response.data.attendances.find(item => currentDay.format('YYYY-MM-DD') === item.day)
+
+          // If current user already attended on current day then set attendance information
+          // Else if current user did not attend on current day then check if current day is weekend or unpaid leave day
+          if (tmpAttendance) {
+            attendance = tmpAttendance
+          } else if (!holiday) {
+            attendance = Object.assign({}, attendance, {
+              off_status: this.currentCompany.breakdays.includes(currentDay.format('dddd').toLowerCase()) ? '' : 'unpaid_leave'
+            })
+          }
+        }
+
+        attendances.push(attendance)
+      }
+
+      this.attendances = attendances
     },
 
     ...mapActions('calendar', [
@@ -148,7 +191,7 @@ export default {
   },
 
   created () {
-    this.getCalendarAttendances()
+    this.getCalendarAttendances().then(response => this.formatAttendances(response))
   }
 }
 </script>
