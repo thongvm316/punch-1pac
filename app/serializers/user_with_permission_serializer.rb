3e420a74a3550f8ca3c6e11088edd84618392a57
Minh Phan <wofi.minh@1pac.vn>
2018-03-16 10:50:54 +0700

    Fix user response and test

--- app/serializers/user_with_permission_serializer.rb
+++ app/serializers/user_with_permission_serializer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class UserWithPermissionSerializer < ApplicationSerializer
-  attributes :id, :email, :name, :avatar_url, :gender, :position, :role
+  attributes :id, :email, :name, :avatar_url, :gender, :position, :role, :created_at
 
   has_many :permissions
+
+  def created_at
+    object.created_at.iso8601
+  end
 end
