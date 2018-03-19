823b9ac35290bc37a8b8ab8d5dc792c3c07e9a2e
Minh Phan <wofi.minh@1pac.vn>
2018-03-19 12:41:22 +0700

    Fix group action permissions

--- app/loyalties/api/v1/groups_loyalty.rb
+++ app/loyalties/api/v1/groups_loyalty.rb
@@ -2,11 +2,15 @@
 
 class Api::V1::GroupsLoyalty < ApplicationLoyalty
   def add_user?
-    return false if record[:user].role == 'member' && record[:user].groups.present?
-    user.manager? && user.groups.include?(record[:group])
+    return false if record[:user].member? && record[:user].groups.present?
+    return true if user.superadmin?
+    return true if user.admin? && user.groups.include?(record[:group])
+    false
   end
 
   def remove_user?
-    user.manager? && user.groups.include?(record)
+    return true if user.superadmin?
+    return true if user.admin? && user.groups.include?(record)
+    false
   end
 end
