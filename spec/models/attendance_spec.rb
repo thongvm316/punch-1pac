6b251444bf0cbbad59bd0791aa6a201afa915a8a
Minh Phan <wofi.minh@1pac.vn>
2018-03-19 12:40:05 +0700

    Correct some validation test

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Attendance, type: :model do
     it { should validate_presence_of(:day) }
     it { should validate_inclusion_of(:attending_status).in_array(%w[attend_ok attend_late]).allow_nil }
     it { should validate_inclusion_of(:leaving_status).in_array(%w[leave_ok leave_early]).allow_nil }
-    it { should validate_inclusion_of(:off_status).in_array(%w[holiday weekend annual_leave]).allow_nil }
+    it { should validate_inclusion_of(:off_status).in_array(%w[annual_leave]).allow_nil }
   end
 
   describe '#scope' do
