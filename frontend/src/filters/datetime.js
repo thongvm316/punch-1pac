cfa32decbcc3b0206621147aacc41d8303aaca04
Minh Phan <wofi.minh@1pac.vn>
2018-03-20 15:57:37 +0700

    Change moment format value

--- frontend/src/filters/datetime.js
+++ frontend/src/filters/datetime.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import moment from '../moment'
 
-Vue.filter('datetime_short', val => moment(val).format('MMM D, hh:mm'))
-Vue.filter('datetime_normal', val => moment(val).format('MMM DD YYYY, HH:mm'))
-Vue.filter('datetime_mmdd', val => moment(val).format('MMM DD'))
+Vue.filter('datetime_short', val => moment(val).format('LLL'))
+Vue.filter('datetime_normal', val => moment(val).format('LLLL'))
+Vue.filter('datetime_mmdd', val => moment(val).format('L'))
