63d074f7343189c9b27d9da88748900ae67c7085
Minh Phan <wofi.minh@1pac.vn>
2018-03-20 15:56:07 +0700

    Implement flash component

---
+++ frontend/src/components/Flash.vue
@@ -0,0 +1,33 @@
+<template>
+  <transition name="fade">
+    <div class="toast toast-success text-center toast-flash" v-if="message">
+      {{ message }}
+    </div>
+  </transition>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'flash',
+
+  computed: {
+    ...mapState('flash', [
+      'message'
+    ])
+  },
+
+  methods: {
+    ...mapActions('flash', [
+      'setFlashMsg'
+    ])
+  },
+
+  watch: {
+    message: function (newMsg) {
+      if (newMsg) setTimeout(() => { this.setFlashMsg(null) }, 2000)
+    }
+  }
+}
+</script>
