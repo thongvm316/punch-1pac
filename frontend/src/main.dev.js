f0d897440b54209e25eb53bbf248eac78d8cd83f
Minh Phan <wofi.minh@1pac.vn>
2018-03-23 12:16:10 +0700

    Config webpack dev env

--- frontend/src/main.dev.js
+++ frontend/src/main.dev.js
@@ -11,8 +11,8 @@ import filterDatetime from './filters/datetime'
 
 import './scss/main.scss'
 
-axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name=csrf-token]').getAttribute('content')
-axios.defaults.baseURL = '/api/v1'
+axios.defaults.baseURL = 'http://namespace_1.localhost:3000/api/v1'
+axios.defaults.headers.common['Authorization'] = 'Bear eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjI4MDc4OTAsImp0aSI6ImMzNmM3YmNiLTA2MzAtNGE1Yy05YjUyLWY3MTUzY2E4ZGMyNyIsInN1YiI6Mn0.7-RIQ1ftgcUTAQr48muGWMP4Z6x9NHsSa50x0oRV5uw'
 axios.defaults.headers.common['Accept'] = 'application/json'
 
 Vue.prototype.$moment = moment
