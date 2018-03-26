726744d2a8a9146a5130d9335e0ff521c194d781
Minh Phan <wofi.minh@1pac.vn>
2018-03-26 16:48:58 +0700

    Fix logic request attendance

--- app/services/request_service.rb
+++ app/services/request_service.rb
@@ -8,14 +8,22 @@ class RequestService
   end
 
   def approve
-    ApplicationRecord.transaction do
-      @req.update!(status: 'approved')
-      @attendance.update!(
+    attendance_params = {}
+    if @req.attended_at.present?
+      attendance_params.merge!(
         attended_at: @req.attended_at,
-        attending_status: AttendanceService.attending_status(@user.company, @req.attended_at, @attendance),
+        attending_status: AttendanceService.attending_status(@user.company, @req.attended_at, @attendance)
+      )
+    end
+    if @req.left_at.present?
+      attendance_params.merge!(
         left_at: @req.left_at,
         leaving_status: AttendanceService.leaving_status(@user.company, @req.left_at, @attendance)
       )
+    end
+    ApplicationRecord.transaction do
+      @req.update!(status: 'approved')
+      @attendance.update!(attendance_params)
     end
   end
 
