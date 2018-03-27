5598d911dd617bae4c794080d63b1418bebcb19f
Minh Phan <wofi.minh@1pac.vn>
2018-03-27 16:13:54 +0700

    Correct permission access specific group

--- app/loyalties/api/v1/groups_loyalty.rb
+++ app/loyalties/api/v1/groups_loyalty.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Api::V1::GroupsLoyalty < ApplicationLoyalty
+  def show?
+    return true if user.superadmin?
+    return true if (user.admin? || user.member?) && user.groups.include?(record)
+    false
+  end
+
   def add_user?
     return true if user.superadmin?
     return true if user.admin? && user.groups.include?(record)
