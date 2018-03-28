4ca0dbeb37888f90be9353951953fc15d6d2dace
Minh Phan <wofi.minh@1pac.vn>
2018-03-28 16:08:11 +0700

    Add column type so migration can rollback

--- db/migrate/20180328025811_change_announcement_columns.rb
+++ db/migrate/20180328025811_change_announcement_columns.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class ChangeAnnouncementColumns < ActiveRecord::Migration[5.1]
   def change
-    remove_column :announcements, :title
+    remove_column :announcements, :title, :string
     change_column :announcements, :content, :string, limit: 500
     add_column :announcements, :due_date, :date, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,5 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-#
-
-require_relative 'seeds/permission'
-
 company = FactoryBot.create(:company)
 user = FactoryBot.create(:user, email: 'wofi.minh@1pac.vn', password: 'password', password_confirmation: 'password', company: company, created_at: Time.current.beginning_of_year)
 superadmin = FactoryBot.create(:user, email: 'example@1pac.vn', password: 'password', password_confirmation: 'password', company: company, role: 'superadmin', created_at: Time.current.beginning_of_year)
