3d64b0a740fe4c98340afa7ccbd33e7a59ccf625
Minh Phan <wofi.minh@1pac.vn>
2018-03-29 15:35:31 +0700

    Specific session ttl

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_1punch_session', domain: :all
+Rails.application.config.session_store :cookie_store, key: '_1punch_session', domain: :all, expire_after: 2592000
