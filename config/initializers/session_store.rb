bb5a43a691bb59bc65d3bdebd338ce8f35a56385
Minh Phan <wofi.minh@1pac.vn>
2018-03-29 15:29:15 +0700

    Reconfig session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_1punch_session'
+Rails.application.config.session_store :cookie_store, key: '_1punch_session', domain: :all
