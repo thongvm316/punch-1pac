a087d7fb2e6195b82e5d6afa5df97544ee7df600
Minh Phan <wofi.minh@1pac.vn>
2018-04-05 22:07:26 +0700

    Return full url image

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,6 +3,10 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :name, :avatar_url, :gender, :position, :role, :language, :created_at
 
+  def avatar_url
+    ActionController::Base.helpers.asset_url(object.avatar_url)
+  end
+
   def created_at
     object.created_at.strftime('%Y-%m-%d')
   end
