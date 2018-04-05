8a012f270be031b6d84be389f6e15a74bba30110
Minh Phan <wofi.minh@1pac.vn>
2018-04-05 17:06:46 +0700

    Remove announcement component

--- frontend/src/store/modules/announcements.js
+++ frontend/src/store/modules/announcements.js
@@ -3,37 +3,14 @@ import axios from 'axios'
 
 const state = {
   pager: {},
-  announcement: {},
   announcements: [],
   headerAnnouncements: []
 }
 
-const getters = {
-  countHeaderAnnouncements (state) {
-    return state.headerAnnouncements.length
-  },
-
-  getFirstFive (state) {
-    return state.headerAnnouncements.slice(0, 5)
-  }
-}
-
 const mutations = {
   [types.READ_ANNOUNCEMENT] (state, id) {
-    const index1 = state.announcements.findIndex(announcement => announcement.id === id)
-    const index2 = state.headerAnnouncements.findIndex(announcement => announcement.id === id)
-    if (state.announcements[index1]) state.announcements[index1].readed = true
-    state.headerAnnouncements.splice(index2, 1)
-    state.announcement.readed = true
-  },
-
-  [types.RECEIVE_ANNOUNCEMENT] (state, payload) {
-    state.announcement = payload
-  },
-
-  [types.RECEIVE_ANNOUNCEMENTS] (state, payload) {
-    state.pager = payload.meta
-    state.announcements = payload.announcements
+    const index = state.headerAnnouncements.findIndex(announcement => announcement.id === id)
+    state.headerAnnouncements.splice(index, 1)
   },
 
   [types.RECEIVE_HEADER_ANNOUNCEMENTS] (state, payload) {
@@ -46,24 +23,6 @@ const actions = {
     return axios.post(`/announcements/${id}/read`)
                 .then(response => {
                   commit(types.READ_ANNOUNCEMENT, id)
-                  return response
-                })
-                .catch(error => { throw error })
-  },
-
-  getAnnouncement ({ commit }, id) {
-    return axios.get(`/announcements/${id}`)
-                .then(response => {
-                  commit(types.RECEIVE_ANNOUNCEMENT, response.data)
-                  return response
-                })
-                .catch(error => { throw error })
-  },
-
-  getAnnouncements ({ commit }, params = {}) {
-    return axios.get('/announcements', { params: params })
-                .then(response => {
-                  commit(types.RECEIVE_ANNOUNCEMENTS, response.data)
                   return response
                 })
                 .catch(error => { throw error })
@@ -82,7 +41,6 @@ const actions = {
 export default {
   namespaced: true,
   state,
-  getters,
   mutations,
   actions
 }
