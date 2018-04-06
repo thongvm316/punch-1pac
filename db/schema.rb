a9e18c466bffdd36571a7ef44ed451ac4cf8042c
Minh Phan <wofi.minh@1pac.vn>
2018-04-06 16:21:07 +0700

    Add national_holiday_id to holidays table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403020013) do
+ActiveRecord::Schema.define(version: 20180406090751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,8 @@ ActiveRecord::Schema.define(version: 20180403020013) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "national_holiday_id"
+    t.index ["company_id", "national_holiday_id"], name: "index_holidays_on_company_id_and_national_holiday_id", unique: true
     t.index ["company_id"], name: "index_holidays_on_company_id"
   end
 
