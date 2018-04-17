a34f2ddacb19ca9e29e7874cdfae026c8ec03b71
Minh Phan <wofi.minh@1pac.vn>
2018-04-17 13:38:06 +0700

    Remove useless files

--- spec/factories/read_request.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :read_request do
-    association(:request, factory: :request, strategy: :build)
-    association(:user, factory: :user, strategy: :build)
-  end
-end
