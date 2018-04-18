79fcf73ae8e2960413e8b4b6fc185444afdca877
DiamondDat <diamond.dat@1pac.vn>
2018-04-18 15:43:32 +0700

    ruby file

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 class NotificationSerializer < ApplicationSerializer
-  attributes :id, :activitable_id, :activitable_type, :kind, :created_at
+  attributes :id, :activitable, :activitable_id, :activitable_type, :kind, :created_at
   belongs_to :user, serializer: UserSerializer
+
+  def activitable
+    if object.activitable_type == 'Request'
+      activitable = Request.find_by(id: object.activitable_id)
+      ActiveModelSerializers::SerializableResource.new(activitable, serializer: RequestSerializer).as_json
+    elsif object.activitable_type == 'Attendance'
+      activitable = Attendance.find_by(id: object.activitable_id)
+      ActiveModelSerializers::SerializableResource.new(activitable, serializer: AttendanceSerializer).as_json
+    end
+  end
 
   def created_at
     object.created_at.iso8601
