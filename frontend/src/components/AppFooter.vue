6637ff4a22e793a13ac6e818cdd0de94bbae3c83
Minh Phan <wofi.minh@1pac.vn>
2018-04-26 18:01:05 +0700

    Add logo and icon

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -1,14 +1,17 @@
 <template>
   <footer class="footer">
     <div class="container grid-xl">
-      <ul class="list-inline">
-        <li>© 2018 1PAC Vietnam, Co.,Ltd</li>
-        <li><a href="/tos">{{ $t('footer.terms') }}</a></li>
-        <li><a href="/privacy">{{ $t('footer.privacy') }}</a></li>
-        <li><a href="/help">{{ $t('footer.help') }}</a></li>
-        <li><a href="/contact">{{ $t('footer.contact') }}</a></li>
-        <li><a href="/about">{{ $t('footer.about') }}</a></li>
-      </ul>
+      <div class="footer-items">
+        <ul class="list-inline">
+          <li><img src="../assets/logo.png"></li>
+          <li><a href="/tos">{{ $t('footer.terms') }}</a></li>
+          <li><a href="/privacy">{{ $t('footer.privacy') }}</a></li>
+          <li><a href="/help">{{ $t('footer.help') }}</a></li>
+          <li><a href="/contact">{{ $t('footer.contact') }}</a></li>
+          <li><a href="/about">{{ $t('footer.about') }}</a></li>
+        </ul>
+        <span>© 2018 1PAC Vietnam, Co.,Ltd</span>
+      </div>
     </div>
   </footer>
 </template>
