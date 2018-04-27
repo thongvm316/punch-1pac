6f04364957ebf226e3755fa81cc96c23a34eeea6
Nguyễn Đức Thuận <beard.thuan@1pac.vn>
2018-04-27 15:32:19 +0700

    add 404 page

--- frontend/src/pages/Error404.vue
+++ frontend/src/pages/Error404.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <h1>404 not found</h1>
-  </div>
+  <main-layout>
+    <div class="container grid-xl">
+      <div class="columns mt-10">
+        <div class="column col-4 col-ml-auto col-mr-auto">
+          <img height="170" width="400" src="../assets/404.png">
+        </div>
+      </div>
+      <h1 class="text-center mt-10">Whoop! Look like something is missing</h1>
+      <p class="text-center mb-0">Sorry, the page you are looking for doesn’t exist.</p>
+      <p class="text-center">Press the button below to get back to previous page.</p>
+      <div class="form-group text-center">
+        <button @click="$router.go(-1)" class="btn btn-primary text-center mt-10" type="button">Go Back</button>
+      </div>
+      </div>
+    </div>
+  </main-layout>
 </template>
+<script>
+import MainLayout from '../layouts/Main'
+export default {
+  components: {
+    MainLayout
+  }
+}
+</script>
