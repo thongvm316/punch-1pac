0ee65533c00bbbd8d2cbe75655d2a8ba908ed9dd
Minh Phan <wofi.minh@1pac.vn>
2018-05-11 12:37:09 +0700

    Rubocop auto

--- config/routes.rb
+++ config/routes.rb
@@ -85,5 +85,5 @@ Rails.application.routes.draw do
   devise_for :admins, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  get '*path'   => 'pages#page_404'
+  get '*path' => 'pages#page_404'
 end
