f48cc60d3d487b002ae7633d1abcb8de2b4aae51
DiamondDat <diamond.dat@1pac.vn>
2018-05-17 15:39:48 +0700

    Add header ajax

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,17 +1,17 @@
-var nav = document.querySelector('.nav');
-var navMenu = nav.querySelector('.navbar-menu');
-var indexNav = document.getElementById('index-nav');
-
 window.addEventListener('DOMContentLoaded', function () {
-  indexHeaderScroll();
 
-  nav.querySelector('.menu-burger').addEventListener('click', toggleNavMenu);
-  nav.querySelector('.close').addEventListener('click', toggleSigninForm);
-  nav.querySelector('.btn-signin').addEventListener('click', toggleSigninForm);
-
-  window.addEventListener('resize', function () {
+  if(nav) {
     indexHeaderScroll();
-  });
+
+    nav.querySelector('.menu-burger').addEventListener('click', toggleNavMenu);
+    nav.querySelector('.close').addEventListener('click', toggleSigninForm);
+    nav.querySelector('.btn-signin').addEventListener('click', toggleSigninForm);
+    nav.querySelector('.btn-submit').addEventListener('click', checkNamespace);
+
+    window.addEventListener('resize', function () {
+      indexHeaderScroll();
+    });
+  }
 });
 
 function toggleSigninForm () {
@@ -19,6 +19,10 @@ function toggleSigninForm () {
 }
 
 function toggleNavMenu () {
+  var nav = document.querySelector('.nav');
+  var navMenu = nav.querySelector('.navbar-menu');
+  var indexNav = document.getElementById('index-nav');
+
   document.querySelector('body').classList.toggle('menu-opened');
   nav.querySelector('.navbar-menu').classList.toggle('open');
   nav.querySelector('.menu-burger').classList.toggle('open');
@@ -31,6 +35,10 @@ function toggleNavMenu () {
 }
 
 function closeNavMenuOnBodyClick () {
+  var nav = document.querySelector('.nav');
+  var navMenu = nav.querySelector('.navbar-menu');
+  var indexNav = document.getElementById('index-nav');
+
   nav.addEventListener('click', function (e) {
     e.stopPropagation();
   });
@@ -47,6 +55,10 @@ function closeNavMenuOnBodyClick () {
 }
 
 function indexHeaderScroll () {
+  var nav = document.querySelector('.nav');
+  var navMenu = nav.querySelector('.navbar-menu');
+  var indexNav = document.getElementById('index-nav');
+
   if(indexNav && !navMenu.classList.contains('open')) {
     window.addEventListener('scroll', function () {
       if(this.pageYOffset > 0) {
@@ -56,4 +68,26 @@ function indexHeaderScroll () {
       }
     });
   }
+}
+
+function checkNamespace () {
+  var nav = document.querySelector('.nav');
+  var navMenu = nav.querySelector('.navbar-menu');
+
+  var xhr = new XMLHttpRequest();
+  var namespace = nav.querySelector('.namespace-input').value;
+  xhr.open("GET", "http://" + namespace + ".builcauhinh.com");
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onload = function(e) {
+    if (this.status === 200) {
+      window.location.href = "http://" + namespace + ".builcauhinh.com";
+    } else if (this.status === 404) {
+      var HTMLtoDoItem = '<span class="text-error mt-8">Company namespace is not found</span>';
+
+      document.querySelector('.input-group').insertAdjacentHTML('afterend', HTMLtoDoItem);
+      nav.querySelector('.namespace-input').reset();
+    }
+  };
+  xhr.send();
 }
