a04b81a9246f474ace527dbe2d9b163961450694
Minh Phan <wofi.minh@1pac.vn>
2018-05-26 17:35:08 +0700

    Config stylelint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -10,16 +10,22 @@ module.exports = {
     browser: true,
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
+  extends: ['standard'],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'html',
+    'prettier'
   ],
   // add your custom rules here
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': ['error', {
+      'printWidth': 200,
+      'semi': false,
+      'singleQuote': true
+    }]
   }
 }
