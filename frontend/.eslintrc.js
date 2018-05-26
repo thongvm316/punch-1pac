8a38d1a592f0bf5df05c1bbcd5cc8ebf654535ce
Minh Phan <wofi.minh@1pac.vn>
2018-05-26 18:17:24 +0700

    Config prettier

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -13,19 +13,19 @@ module.exports = {
   extends: ['standard'],
   // required to lint *.vue files
   plugins: [
-    'html',
-    'prettier'
+    'html'
   ],
   // add your custom rules here
   rules: {
     // allow async-await
+    'curly': 'off',
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': ['error', {
-      'printWidth': 200,
-      'semi': false,
-      'singleQuote': true
+    'space-before-function-paren': ['error', {
+      "anonymous": "never",
+      "named": "never",
+      "asyncArrow": "always"
     }]
   }
 }
