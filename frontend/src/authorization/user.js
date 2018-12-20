6fa6504f12bfc074afe5f13fbd34c8c76d4e14e6
moon-hai <moon.hai@1pac.vn>
2018-12-20 11:40:47 +0700

    user permission

--- frontend/src/authorization/user.js
+++ frontend/src/authorization/user.js
@@ -3,7 +3,7 @@ import Abstract from './abstract'
 export default class User extends Abstract {
   canEdit() {
     if (this.currentUser.owner) return true
-    if (this.currentUser.role === 'superadmin' && !this.record.owner) return true
+    if (this.currentUser.role === 'superadmin' && (!this.record.owner && this.record.role !== 'superadmin')) return true
     if (this.currentUser.role === 'admin' && this.record.role === 'member') return true
     if (this.currentUser.id === this.record.id) return true
     return false
@@ -25,7 +25,7 @@ export default class User extends Abstract {
   canActivate() {
     if (this.record.owner) return false
     if (this.currentUser.owner) return true
-    if (this.currentUser.role === 'superadmin') return true
+    if (this.currentUser.role === 'superadmin' && (!this.record.owner && this.record.role !== 'superadmin')) return true
     if (this.currentUser.role === 'admin' && this.record.role === 'member') return true
     return false
   }
@@ -33,7 +33,7 @@ export default class User extends Abstract {
   canDeactivate() {
     if (this.record.owner) return false
     if (this.currentUser.owner) return true
-    if (this.currentUser.role === 'superadmin') return true
+    if (this.currentUser.role === 'superadmin' && (!this.record.owner && this.record.role !== 'superadmin')) return true
     if (this.currentUser.role === 'admin' && this.record.role === 'member') return true
     return false
   }
