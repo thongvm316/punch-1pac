0932ae23ba76eeda16295e429bd25f18b82b3b99
nguyenthanhcong <nguyenthanhcong101096@gmail.com>
2019-12-25 13:55:08 +0700

    document export data

---
+++ app/queries/user_query.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class UserQuery
+  def self.row_data(attendance)
+    [
+      attendance.day,
+      attendance.attended_time,
+      attendance.left_time,
+      attendance.attending_status == 'attend_late' ? '✓' : '-',
+      attendance.leaving_status   == 'leave_early' ? '✓' : '-',
+      "#{attendance.working_hours.to_i / 3600}h#{attendance.working_hours.to_i % 3600 / 60}m"
+    ]
+  end
+
+  def self.report_csv(attendances, params = {})
+    date = Date.parse(params[:date])
+    (date.beginning_of_month..date.end_of_month).to_a.each_with_object([]) do |day, arr|
+      attendance = attendances.find_by(day: day)
+      arr << (attendance ? row_data(attendance) : [day])
+    end
+  end
+end
