f9d58962d21d97e244a70344fb884efe4de1f295
moon-hai <moon.hai@1pac.vn>
2019-12-26 17:19:54 +0700

    add more test case

--- frontend/test/unit/specs/AllowedIpForm.spec.js
+++ frontend/test/unit/specs/AllowedIpForm.spec.js
@@ -1,19 +1,28 @@
+import { shallowMount } from '@vue/test-utils'
+
 import store from '@/store'
 import i18n from '@/locale'
 import AllowedIpForm from '@/components/AllowedIpForm'
-import { shallowMount } from '@vue/test-utils'
 
 const wrapper = shallowMount(AllowedIpForm, {
   i18n,
-  store,
-  propsData: {
-    targetIp: '192.168.110.1'
-  }
+  store
 })
 
 describe('AllowedIpForm.vue', () => {
-  it('should display create allowed ip button', () => {
+  it('should display AllowedIpForm Component', () => {
     expect(wrapper.exists()).toBe(true)
+  })
+
+  it('should display create allowed ip button', () => {
+    expect(wrapper.find({ ref: 'createAllowedIpButton' }).exists()).toBe(true)
+    expect(wrapper.find({ ref: 'editAllowedIpButton' }).exists()).toBe(false)
+  })
+
+  it('should display update allowed ip button', async () => {
+    wrapper.setProps({ targetIp: '192.168.110.1' })
+    await wrapper.vm.$forceUpdate()
+
     expect(wrapper.props().targetIp).toBe('192.168.110.1')
     expect(wrapper.find({ ref: 'createAllowedIpButton' }).exists()).toBe(false)
     expect(wrapper.find({ ref: 'editAllowedIpButton' }).exists()).toBe(true)
