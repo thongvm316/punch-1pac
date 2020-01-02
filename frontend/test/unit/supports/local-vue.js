c01f422f6b7034fa1fe5c3f374f65dc08f36da38
moon-hai <moon.hai@1pac.vn>
2020-01-02 09:42:57 +0700

    setup axios prototype to local vue

--- frontend/test/unit/supports/local-vue.js
+++ frontend/test/unit/supports/local-vue.js
@@ -2,13 +2,18 @@ import { createLocalVue } from '@vue/test-utils'
 
 import moment from '@/moment'
 import authorization from '@/authorization'
+import axios from 'axios'
 
 import currentUser from '@/mixins/current-user'
 import pageAuthorization from '@/mixins/page-authorization'
 
+axios.defaults.baseURL = '/frontend/test/unit/supports/api'
+axios.defaults.headers.common['Accept'] = 'application/json'
+
 const localVue = createLocalVue()
 localVue.prototype.$moment = moment
 localVue.prototype.$auth = authorization
+localVue.prototype.$axios = axios
 localVue.mixin(currentUser)
 localVue.mixin(pageAuthorization)
 
