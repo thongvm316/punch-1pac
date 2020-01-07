443d2ecb2df4e02c3c87e4347e74ad2cad306dc5
xuan.tuan <xuan.tuan@1pac.vn>
2020-01-07 11:52:19 +0700

    remove useless code remindPunchIn

--- frontend/test/unit/specs/components/RemindPunchIn.spec.js
+++ frontend/test/unit/specs/components/RemindPunchIn.spec.js
@@ -11,54 +11,52 @@ describe('RemindPunchIn.vue', () => {
 
   afterEach(() => { wrapper.vm.$destroy() })
 
-  describe('when RemindPunchIn was mounted ', () => {
-    describe('when user remember to punch in all day', () => {
-      beforeEach(() => {
-        wrapper = shallowMount(RemindPunchIn, {
-          ...wrapperOps,
-          computed: {
-            currentUser() {
-              return { forgot_punch_in_days_in_month: [] }
-            }
+  describe('when user remember to punch in all day', () => {
+    beforeEach(() => {
+      wrapper = shallowMount(RemindPunchIn, {
+        ...wrapperOps,
+        computed: {
+          currentUser() {
+            return { forgot_punch_in_days_in_month: [] }
           }
-        })
+        }
+      })
 
-        it('should render correctly', () => {
-          expect(wrapper.exists()).toBeTruthy()
-          expect(wrapper.isVueInstance()).toBeTruthy()
-        })
+      it('should render correctly', () => {
+        expect(wrapper.exists()).toBeTruthy()
+        expect(wrapper.isVueInstance()).toBeTruthy()
+      })
 
-        it('should not show the reminder toast', () => {
-          expect(wrapper.isVisible()).toBeeFalsy()
-        })
+      it('should not show the reminder toast', () => {
+        expect(wrapper.isVisible()).toBeeFalsy()
+      })
+    })
+  })
+
+  describe('when user have forgot punch in some day', () => {
+    beforeEach(() => {
+      wrapper = shallowMount(RemindPunchIn, {
+        ...wrapperOps,
+        computed: {
+          currentUser() {
+            return { forgot_punch_in_days_in_month: ['2018-05-07', '2018-05-08', '2018-06-20'] }
+          }
+        },
+        methods: {
+          close
+        }
       })
     })
 
-    describe('when user have forgot punch in some day', () => {
-      beforeEach(() => {
-        wrapper = shallowMount(RemindPunchIn, {
-          ...wrapperOps,
-          computed: {
-            currentUser() {
-              return { forgot_punch_in_days_in_month: ['2018-05-07', '2018-05-08', '2018-06-20'] }
-            }
-          },
-          methods: {
-            close
-          }
-        })
-      })
+    it('should show the remind punch in toast', () => {
+      expect(wrapper.isVisible()).toBeTruthy()
+    })
 
-      it('should show the remind punch in toast', () => {
-        expect(wrapper.isVisible()).toBeTruthy()
-      })
+    it('should call close when click close button', async () => {
+      wrapper.find({ ref: 'btnCloseToast' }).trigger('click')
+      await wrapper.vm.$nextTick()
 
-      it('should call close when click close button', async () => {
-        wrapper.find({ref: 'btnCloseToast'}).trigger('click')
-        await wrapper.vm.$nextTick()
-
-        expect(close).toHaveBeenCalled()
-      })
+      expect(close).toHaveBeenCalled()
     })
   })
 })
