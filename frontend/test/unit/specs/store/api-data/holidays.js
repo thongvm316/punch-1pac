4427a7d5cccd123da8d963bc48739b87c857afc3
moon-hai <moon.hai@1pac.vn>
2020-01-08 11:27:01 +0700

    add unit test for company-holidays store

---
+++ frontend/test/unit/specs/store/api-data/holidays.js
@@ -0,0 +1,22 @@
+export const holidaysData = () => {
+  return [
+    {
+      ended_at: '2020-01-09',
+      id: 1,
+      name: 'quo',
+      started_at: '2020-01-05',
+    },
+    {
+      ended_at: '2020-01-08',
+      id: 2,
+      name: 'ut',
+      started_at: '2020-01-05',
+    },
+    {
+      ended_at: '2020-01-18',
+      id: 3,
+      name: 'saepe',
+      started_at: '2020-01-02',
+    }
+  ]
+}
