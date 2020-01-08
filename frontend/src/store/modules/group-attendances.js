d2ad441c37051d85cc2ab773445aaf138037a667
moon-hai <moon.hai@1pac.vn>
2020-01-08 14:54:19 +0700

    group attendance store unit test

--- frontend/src/store/modules/group-attendances.js
+++ frontend/src/store/modules/group-attendances.js
@@ -1,18 +1,15 @@
 import * as types from '../mutation-types.js'
-import axios from 'axios'
+import callApi from '../api-caller'
 
 const state = {
-  params: {},
   pager: {},
   attendances: []
 }
 
 const getters = {
-  filterAttendances(state) {
-    return function(query) {
-      const regex = new RegExp(`${query.trim()}`, 'gi')
-      return query ? state.attendances.filter(attendance => (attendance.user.name.match(regex)) || (attendance.user.email.match(regex))) : state.attendances
-    }
+  filterAttendances: state => query => {
+    const regex = new RegExp(`${query.trim()}`, 'gi')
+    return query ? state.attendances.filter(attendance => (attendance.user.name.match(regex)) || (attendance.user.email.match(regex))) : state.attendances
   }
 }
 
@@ -25,8 +22,7 @@ const mutations = {
 
 const actions = {
   getAttendances({ commit, state }, params = {}) {
-    return axios
-      .get('/attendances', { params: Object.assign(state.params, params) })
+    return callApi({ method: 'get', url: '/attendances', params })
       .then(response => {
         commit(types.RECEIVE_GROUP_ATTENDANCES, response.data)
         return response
