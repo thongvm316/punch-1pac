012fdcd900b7f04cdd578e7cf3a1d8bddf18a56e
moon-hai <moon.hai@1pac.vn>
2020-01-08 13:50:53 +0700

    Flash store unit test

---
+++ frontend/test/unit/specs/store/api-data/flash.api.js
@@ -0,0 +1,14 @@
+export const flashData = () => {
+  return {
+    success: {
+      message: 'Congratulation!!!',
+      type: 'success',
+      timeout: 5000
+    },
+
+    error: {
+      message: 'Error!!!',
+      type: 'error'
+    }
+  }
+}
---
+++ frontend/test/unit/specs/store/modules/flash.spec.js
@@ -0,0 +1,38 @@
+import flash from '@/store/modules/flash'
+import callApi from '@/store/api-caller'
+import { flashData } from '../api-data/flash.api.js'
+jest.mock('@/store/api-caller')
+
+const { state, mutations, actions } = flash
+const commit = jest.fn()
+
+describe('mutations', () => {
+  let payload
+
+  it('should SET_FLASH_MESSAGE #success', () => {
+    payload = flashData().success
+    mutations.SET_FLASH_MESSAGE(state, payload)
+
+    expect(state.message).toEqual('Congratulation!!!')
+    expect(state.type).toEqual('success')
+    expect(state.timeout).toEqual(5000)
+  })
+
+  it('should SET_FLASH_MESSAGE #error', () => {
+    payload = flashData().error
+    mutations.SET_FLASH_MESSAGE(state, payload)
+
+    expect(state.message).toEqual('Error!!!')
+    expect(state.type).toEqual('error')
+    expect(state.timeout).toEqual(3000)
+  })
+})
+
+describe('actions', () => {
+  it('should commit SET_FLASH_MESSAGE', () => {
+    const flash = flashData().error
+    actions.setFlashMsg({ commit }, flash)
+
+    expect(commit).toHaveBeenCalledWith('SET_FLASH_MESSAGE', flash)
+  })
+})
