ade841e98ddfe97f9006e052f1a3295afec1e396
Ngan B. Nguyen <notngan@gmail.com>
2020-01-10 16:43:10 +0700

    lazy load routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,61 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Dashboard from '../pages/Dashboard'
-import Attendances from '../pages/Attendances'
-import Requests from '../pages/Requests'
-
-import GroupRequests from '../pages/GroupRequests'
-import GroupAttendances from '../pages/GroupAttendances'
-import GroupReport from '../pages/GroupReport'
-import GroupReportPersonal from '../pages/GroupReportPersonal'
-import Groups from '../pages/Groups'
-import Group from '../pages/Group'
-
-import UserSettingsProfile from '../pages/UserSettingsProfile'
-import UserSettingsPassword from '../pages/UserSettingsPassword'
-import UserSettingsSecurity from '../pages/UserSettingsSecurity'
-import CompanySettingsProfile from '../pages/CompanySettingsProfile'
-import CompanySettingsUsers from '../pages/CompanySettingsUsers'
-import CompanySettingsUsersAdd from '../pages/CompanySettingsUsersAdd'
-import CompanySettingsUsersAddMulti from '../pages/CompanySettingsUsersAddMulti'
-import CompanySettingsBusinessDays from '../pages/CompanySettingsBusinessDays'
-import CompanySettingsHolidays from '../pages/CompanySettingsHolidays'
-import CompanySettingsAllowedIPs from '../pages/CompanySettingsAllowedIPs'
-
-import Error404 from '../pages/Error404'
-
 Vue.use(Router)
 
 const appName = window.initialStates().meta.app_name
+
+const lazyLoadRoute = pageName => {
+  return () => import(/* webpackChunkName: "[request]" */ `@/pages/${pageName}`)
+}
 
 const router = new Router({
   mode: 'history',
   linkActiveClass: 'active',
   routes: [
-    { path: '/dashboard', name: 'dashboard', component: Dashboard, meta: { title: `Dashboard - ${appName}` } },
-    { path: '/attendances', name: 'attendances', component: Attendances, meta: { title: `Attendances - ${appName}` } },
-    { path: '/requests', name: 'requests', component: Requests, meta: { title: `Requests - ${appName}` } },
-    { path: '/groups/:id/requests', name: 'group-requests', component: GroupRequests, meta: { title: `Group Requests - ${appName}` } },
-    { path: '/groups/:id/attendances', name: 'group-attendances', component: GroupAttendances, meta: { title: `Group Attendances - ${appName}` } },
-    { path: '/groups/:id/report', name: 'group-report', component: GroupReport, meta: { title: `Group Report - ${appName}` } },
-    { path: '/groups/:id/users/:user_id/report', name: 'group-report-personal', component: GroupReportPersonal, meta: { title: `Group Report Personal - ${appName}` } },
-    { path: '/groups/:id/users', name: 'group', component: Group, meta: { title: `Group - ${appName}` } },
+    { path: '/dashboard', name: 'dashboard', component: lazyLoadRoute('Dashboard'), meta: { title: `Dashboard - ${appName}` } },
+    { path: '/attendances', name: 'attendances', component: lazyLoadRoute('Attendances'), meta: { title: `Attendances - ${appName}` } },
+    { path: '/requests', name: 'requests', component: lazyLoadRoute('Requests'), meta: { title: `Requests - ${appName}` } },
+    { path: '/groups/:id/requests', name: 'group-requests', component: lazyLoadRoute('GroupRequests'), meta: { title: `Group Requests - ${appName}` } },
+    { path: '/groups/:id/attendances', name: 'group-attendances', component: lazyLoadRoute('GroupAttendances'), meta: { title: `Group Attendances - ${appName}` } },
+    { path: '/groups/:id/report', name: 'group-report', component: lazyLoadRoute('GroupReport'), meta: { title: `Group Report - ${appName}` } },
+    { path: '/groups/:id/users/:user_id/report', name: 'group-report-personal', component: lazyLoadRoute('GroupReportPersonal'), meta: { title: `Group Report Personal - ${appName}` } },
+    { path: '/groups/:id/users', name: 'group', component: lazyLoadRoute('Group'), meta: { title: `Group - ${appName}` } },
     { path: '/groups/:id', redirect: '/groups/:id/users' },
-    { path: '/groups', name: 'groups', component: Groups, meta: { title: `Groups - ${appName}` } },
-    { path: '/settings/profile', name: 'user-settings-profile', component: UserSettingsProfile, meta: { title: `User Settings Profile - ${appName}` } },
-    { path: '/settings/password', name: 'user-settings-password', component: UserSettingsPassword, meta: { title: `User Settings Password - ${appName}` } },
-    { path: '/settings/security', name: 'user-settings-security', component: UserSettingsSecurity, meta: { title: `User Settings Security - ${appName}` } },
+    { path: '/groups', name: 'groups', component: lazyLoadRoute('Groups'), meta: { title: `Groups - ${appName}` } },
+    { path: '/settings/profile', name: 'user-settings-profile', component: lazyLoadRoute('UserSettingsProfile'), meta: { title: `User Settings Profile - ${appName}` } },
+    { path: '/settings/password', name: 'user-settings-password', component: lazyLoadRoute('UserSettingsPassword'), meta: { title: `User Settings Password - ${appName}` } },
+    { path: '/settings/security', name: 'user-settings-security', component: lazyLoadRoute('UserSettingsSecurity'), meta: { title: `User Settings Security - ${appName}` } },
     { path: '/settings', redirect: '/settings/profile' },
-    { path: '/company/settings/profile', name: 'company-settings-profile', component: CompanySettingsProfile, meta: { title: `Company Settings Profile - ${appName}` } },
-    { path: '/company/settings/users', name: 'company-settings-users', component: CompanySettingsUsers, meta: { title: `Company Settings Users - ${appName}` } },
-    { path: '/company/settings/users/add', name: 'company-settings-users-add', component: CompanySettingsUsersAdd, meta: { title: `Company Settings Add User - ${appName}` } },
-    { path: '/company/settings/users/add-multi', name: 'company-settings-users-add-multi', component: CompanySettingsUsersAddMulti, meta: { title: `Company Settings Add Multiple Users - ${appName}` } },
-    { path: '/company/settings/business-days', name: 'company-settings-business-days', component: CompanySettingsBusinessDays, meta: { title: `Company Settings Business Days - ${appName}` } },
-    { path: '/company/settings/holidays', name: 'company-settings-holidays', component: CompanySettingsHolidays, meta: { title: `Company Settings Holidays - ${appName}` } },
-    { path: '/company/settings/allowed-ips', name: 'company-settings-allowed-ips', component: CompanySettingsAllowedIPs, meta: { title: `Company Settings Allowed IPs - ${appName}` } },
+    { path: '/company/settings/profile', name: 'company-settings-profile', component: lazyLoadRoute('CompanySettingsProfile'), meta: { title: `Company Settings Profile - ${appName}` } },
+    { path: '/company/settings/users', name: 'company-settings-users', component: lazyLoadRoute('CompanySettingsUsers'), meta: { title: `Company Settings Users - ${appName}` } },
+    { path: '/company/settings/users/add', name: 'company-settings-users-add', component: lazyLoadRoute('CompanySettingsUsersAdd'), meta: { title: `Company Settings Add User - ${appName}` } },
+    { path: '/company/settings/users/add-multi', name: 'company-settings-users-add-multi', component: lazyLoadRoute('CompanySettingsUsersAddMulti'), meta: { title: `Company Settings Add Multiple Users - ${appName}` } },
+    { path: '/company/settings/business-days', name: 'company-settings-business-days', component: lazyLoadRoute('CompanySettingsBusinessDays'), meta: { title: `Company Settings Business Days - ${appName}` } },
+    { path: '/company/settings/holidays', name: 'company-settings-holidays', component: lazyLoadRoute('CompanySettingsHolidays'), meta: { title: `Company Settings Holidays - ${appName}` } },
+    { path: '/company/settings/allowed-ips', name: 'company-settings-allowed-ips', component: lazyLoadRoute('CompanySettingsAllowedIPs'), meta: { title: `Company Settings Allowed IPs - ${appName}` } },
     { path: '/company/settings', redirect: '/company/settings/profile' },
-    { path: '/404', name: 'error-404', component: Error404, meta: { title: '404 Page not found' } },
+    { path: '/404', name: 'error-404', component: lazyLoadRoute('Error404'), meta: { title: '404 Page not found' } },
     { path: '*', redirect: '/404' }
   ]
 })
