53054d079c7d3072c7b29302dacb44b10540cfdf
moon-hai <moon.hai@1pac.vn>
2020-01-13 10:07:21 +0700

    spec test for user authorization

---
+++ frontend/test/unit/specs/authorization/support/user.js
@@ -0,0 +1,15 @@
+export const targetUser = () => {
+  return {
+    role: 'member',
+    owner: false,
+    id: 1
+  }
+}
+
+export const currUser = () => {
+  return {
+    role: 'admin',
+    owner: false,
+    id: 0
+  }
+}
