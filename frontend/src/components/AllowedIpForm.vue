a47130958c2a7c084a1a52b0de4cd79e67f9bd87
moon-hai <moon.hai@1pac.vn>
2020-01-14 17:51:31 +0700

    confirm dialog before remove ip address

--- frontend/src/components/AllowedIpForm.vue
+++ frontend/src/components/AllowedIpForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="form-group" :class="{ 'has-error': errors.ip_address}">
+    <div class="form-group">
       <label class="form-label">{{ $t('company.allowedIPs.labels.ipAddress') }}</label>
       <input class="form-input" type="text" v-model.trim="$v.params.$model" :class="{ 'is-error': $v.params.$error, 'is-success': $v.params.isValid }">
       <p class="form-input-hint text-error" v-if="$v.params.$error">
@@ -15,23 +15,22 @@
         class="btn btn-success btn-submit"
         @click="localAddIp"
         v-if="!targetIp"
-        :disabled="!$v.params.isValid">{{ $t('company.allowedIPs.btn.submit') }}</button>
+        :disabled="!$v.params.isValid || params === ''">{{ $t('company.allowedIPs.btn.submit') }}</button>
       <button
         ref="editAllowedIpButton"
         class="btn btn-success btn-submit"
         @click="localEditIp"
         v-if="targetIp"
-        :disabled="!$v.params.isValid">{{ $t('company.allowedIPs.btn.save') }}</button>
+        :disabled="!$v.params.isValid || params === targetIp.ip_address">{{ $t('company.allowedIPs.btn.save') }}</button>
     </div>
   </div>
 </template>
 
 <script>
-import { CLEAR_IP_ERRORS } from '../store/mutation-types'
 import { mapState, mapActions, mapMutations } from 'vuex'
+import { CLEAR_IP_ERRORS } from '../store/mutation-types'
 import handleSuccess from '../mixins/handle-success'
-import { validationMixin } from 'vuelidate'
-import { required } from 'vuelidate/lib/validators'
+import allowedIP from '../validations/allowed-ip'
 
 export default {
   name: 'allowed-ip-form',
@@ -40,7 +39,7 @@ export default {
     targetIp: Object
   },
 
-  mixins: [validationMixin, handleSuccess],
+  mixins: [allowedIP, handleSuccess],
 
   data() {
     return {
@@ -50,16 +49,6 @@ export default {
         message: ''
       },
       params: ''
-    }
-  },
-
-  validations: {
-    params: {
-      required,
-      isValid(params) {
-        let ipRegex = /^(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)$/
-        return ipRegex.test(params)
-      }
     }
   },
 
@@ -93,10 +82,7 @@ export default {
 
   created() {
     if (Object.keys(this.errors).length) this[CLEAR_IP_ERRORS]()
-
-    if (this.targetIp) {
-      this.params = this.targetIp.ip_address
-    }
+    if (this.targetIp) this.params = this.targetIp.ip_address
   }
 }
 </script>
